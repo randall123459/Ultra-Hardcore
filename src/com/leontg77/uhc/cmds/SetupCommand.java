aeb738dd80d4e8b9cfb6c71d7a8e6d182ee140cd
LeonTG77 <leontg77@gmail.com>
2015-08-01 00:20:48 +0200

    Working on a new setup command, updating progress

--- src/com/leontg77/uhc/cmds/SetupCommand.java
+++ src/com/leontg77/uhc/cmds/SetupCommand.java
@@ -1,37 +1,41 @@
 package com.leontg77.uhc.cmds;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.leontg77.uhc.Main;
-
 public class SetupCommand implements CommandExecutor {
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, final String[] args) {
-		if (!(sender instanceof Player)) {
-			sender.sendMessage(ChatColor.RED + "Only players can tell their team their location.");
-			return true;
-		}
-		
-		Player player = (Player) sender;
-		
-		if (cmd.getName().equalsIgnoreCase("teamloc")) {
-			if (Main.spectating.contains(player.getName())) {
-				player.sendMessage(ChatColor.RED + "You are not allowed to do that while spectating.");
-	        } else if (player.getScoreboard().getEntryTeam(player.getName()) == null) { 
-	        	player.sendMessage(ChatColor.RED + "You are not on a team.");
-			} else {
-				for (String s : player.getScoreboard().getEntryTeam(player.getName()).getEntries()) {
-					Player team = Bukkit.getServer().getPlayer(s);
-					
-					if (team instanceof Player) {
-						((Player) team).sendMessage(player.getScoreboard().getEntryTeam(player.getName()).getPrefix() + "[Team]" + " §f" + player.getName() + "'s location: world: " + player.getWorld().getEnvironment().name().replaceAll("_", " ").toLowerCase().replaceAll("normal", "overworld") + ", x: " + player.getLocation().getBlockX() + ", y: " + player.getLocation().getBlockY() + ", z: " + player.getLocation().getBlockZ());
-					}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("setup")) {
+			if (sender.hasPermission("uhc.setup")) {
+				if (args.length == 0) {
+					sender.sendMessage(ChatColor.RED + "Usage: /setup <radius> [world]");
+					return true;
 				}
+				
+				int radius;
+				
+				try {
+					radius = Integer.parseInt(args[0]);
+				} catch (Exception e) {
+					sender.sendMessage(ChatColor.RED + "Invaild radius.");
+					return true;
+				}
+				
+				if (args.length == 1) {
+					if (sender instanceof Player) {
+						Player player = (Player) sender;
+						
+					} else {
+						sender.sendMessage(ChatColor.RED + "Only players can generate their own world.");
+					}
+					return true;
+				}
+			} else {
+				sender.sendMessage(ChatColor.RED + "You do not have permission for this command.");
 			}
 		}
 		return true;
