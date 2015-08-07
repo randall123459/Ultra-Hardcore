9b42b2f09db818d6dc743ab0a5d300e7ec34c1c4
LeonTG77 <leontg77@gmail.com>
2015-08-07 10:54:18 +0200

    SpecInfo would cancel cutclean if it counted the blocks next to it

--- src/com/leontg77/uhc/scenario/types/CutClean.java
+++ src/com/leontg77/uhc/scenario/types/CutClean.java
@@ -102,37 +102,35 @@ public class CutClean extends Scenario implements Listener {
 				return;
 			}
 			
-			if (SpecInfoListener.locs.contains(event.getBlock().getLocation())) {
-				return;
-			}
-			
-			Player player = event.getPlayer();
-			int amount = 0;
-			Location loc = event.getBlock().getLocation();
-			
-			for (int x = loc.getBlockX() - 1; x <= loc.getBlockX() + 1; x++) {
-				for (int y = loc.getBlockY() - 1; y <= loc.getBlockY() + 1; y++) {
-					for (int z = loc.getBlockZ() - 1; z <= loc.getBlockZ() + 1; z++) {
-						if (loc.getWorld().getBlockAt(x, y, z).getType() == Material.GOLD_ORE) {
-							amount++;
-							SpecInfoListener.locs.add(loc.getWorld().getBlockAt(x, y, z).getLocation());
+			if (!SpecInfoListener.locs.contains(event.getBlock().getLocation())) {
+				Player player = event.getPlayer();
+				int amount = 0;
+				Location loc = event.getBlock().getLocation();
+				
+				for (int x = loc.getBlockX() - 1; x <= loc.getBlockX() + 1; x++) {
+					for (int y = loc.getBlockY() - 1; y <= loc.getBlockY() + 1; y++) {
+						for (int z = loc.getBlockZ() - 1; z <= loc.getBlockZ() + 1; z++) {
+							if (loc.getWorld().getBlockAt(x, y, z).getType() == Material.GOLD_ORE) {
+								amount++;
+								SpecInfoListener.locs.add(loc.getWorld().getBlockAt(x, y, z).getLocation());
+							}
 						}
 					}
 				}
-			}
-			
-			if (SpecInfoListener.totalG.containsKey(player.getName())) {
-				SpecInfoListener.totalG.put(player.getName(), SpecInfoListener.totalG.get(player.getName()) + amount);
-			} else {
-				SpecInfoListener.totalG.put(player.getName(), amount);
-			}
-			
-			for (Player online : PlayerUtils.getPlayers()) {
-				if (Main.spectating.contains(online.getName())) {
-					online.sendMessage("[§9S§f] §7" + player.getName() + "§f:§6GOLD §f[V:§6" + amount + "§f] [T:§6" + SpecInfoListener.totalG.get(player.getName()) + "§f]");
+				
+				if (SpecInfoListener.totalG.containsKey(player.getName())) {
+					SpecInfoListener.totalG.put(player.getName(), SpecInfoListener.totalG.get(player.getName()) + amount);
+				} else {
+					SpecInfoListener.totalG.put(player.getName(), amount);
 				}
+				
+				for (Player online : PlayerUtils.getPlayers()) {
+					if (Main.spectating.contains(online.getName())) {
+						online.sendMessage("[§9S§f] §7" + player.getName() + "§f:§6GOLD §f[V:§6" + amount + "§f] [T:§6" + SpecInfoListener.totalG.get(player.getName()) + "§f]");
+					}
+				}
+				amount = 0;
 			}
-			amount = 0;
 			
 			event.setCancelled(true);
 			BlockUtils.blockCrack(event.getPlayer(), block.getLocation(), 14);
