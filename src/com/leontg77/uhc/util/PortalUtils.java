d013f37b83a3c2a225973c8c5cfa01c405726fb3
LeonTG77 <leontg77@gmail.com>
2015-08-12 01:44:55 +0200

    Fixed author and removed useless methods

--- src/com/leontg77/uhc/util/PortalUtils.java
+++ src/com/leontg77/uhc/util/PortalUtils.java
@@ -1,60 +1,15 @@
 package com.leontg77.uhc.util;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.TravelAgent;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 
 /**
- * Nether utilities class
- * @author LeonTG77
+ * Portal utilities class
+ * @author D4mnX
  */
 public class PortalUtils {
-	
-	/**
-	 * Find a possible portal location for an entity.
-	 * @param entity the entity.
-	 * @param from the from portal.
-	 * @param travelAgent the portal travel agent.
-	 * @return The possible portal location, null if not found.
-	 */
-    public static Location getPossiblePortalLocation(Entity entity, Location from, TravelAgent travelAgent) {
-        String fromWorldName = from.getWorld().getName();
-
-        String targetWorldName;
-        if (from.getWorld().getEnvironment() == World.Environment.NETHER) {
-            if (!fromWorldName.endsWith("_nether")) {
-                worldError(entity);
-                return null;
-            }
-
-            targetWorldName = fromWorldName.substring(0, fromWorldName.length() - 7);
-        } else if (from.getWorld().getEnvironment() == World.Environment.NORMAL) {
-            if (!isPortal(Material.PORTAL, from)) {
-                return null;
-            }
-
-            targetWorldName = fromWorldName + "_nether";
-        } else {
-            return null;
-        }
-
-        World targetWorld = Bukkit.getWorld(targetWorldName);
-        if (targetWorld == null) {
-            worldError(entity);
-            return null;
-        }
-
-        Location to = new Location(targetWorld, from.getX(), from.getY(), from.getZ(), from.getYaw(), from.getPitch());
-        to = travelAgent.findOrCreate(to);
-        return to;
-    }
 	
     /**
      * Check if an location has a portal near itself.
@@ -81,38 +36,5 @@ public class PortalUtils {
         }
 
         return false;
-    }
-    
-    /**
-     * Sends an error to an entity if the portal didn't work.
-     * @param entity the entity.
-     */
-    private static void worldError(Entity entity) {
-        if (entity.getWorld().getEnvironment() == World.Environment.NORMAL) {
-            sendMessage(entity,
-                    "No world is linked to this portal. Nether is most likely off.",
-                    "Please check the match post. If nether is stated",
-                    "as \"enabled\" there, please inform an operator."
-            );
-        } else {
-            sendMessage(entity,
-                    "No world is linked to this portal. How did this happen?",
-                    "You are already in the nether, this is unexpected.",
-                    "Please immediately inform an operator."
-            );
-        }
-    }
-
-    /**
-     * Sends messages to an entity.
-     * @param entity the entity.
-     * @param messages messages to send.
-     */
-    private static void sendMessage(Entity entity, String... messages) {
-        if (entity.getType() == EntityType.PLAYER) {
-            for (String message : messages) {
-                ((Player) entity).sendMessage(message);
-            }
-        }
     }
 }
