ff31a8905e5ffca2b5846a0b246c7ce1b923bad9
LeonTG77 <leontg77@gmail.com>
2015-09-04 10:02:18 +0200

    Added getMobName(Entity)

--- src/com/leontg77/uhc/utils/NameUtils.java
+++ src/com/leontg77/uhc/utils/NameUtils.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffectType;
 
@@ -92,6 +93,139 @@ public class NameUtils {
 		}
 	}
 	
+	public static String getMobName(Entity entity) {
+		switch (entity.getType()) {
+		case ARMOR_STAND:
+			return "Armor Stand";
+		case ARROW:
+			return "Arrow";
+		case BAT:
+			return "Bat";
+		case BLAZE:
+			return "Blaze";
+		case BOAT:
+			return "Boat";
+		case CAVE_SPIDER:
+			return "Cave Spider";
+		case CHICKEN:
+			return "Chicken";
+		case COW:
+			return "Cow";
+		case CREEPER:
+			return "Creeper";
+		case DROPPED_ITEM:
+			return "Dropped Item";
+		case EGG:
+			return "Egg";
+		case ENDERMAN:
+			return "Enderman";
+		case ENDERMITE:
+			return "Endermite";
+		case ENDER_CRYSTAL:
+			return "Ender Crystal";
+		case ENDER_DRAGON:
+			return "Ender Dragon";
+		case ENDER_PEARL:
+			return "Ender Pearl";
+		case ENDER_SIGNAL:
+			return "Ender Signal";
+		case EXPERIENCE_ORB:
+			return "Exp. Orb";
+		case FALLING_BLOCK:
+			return "Falling Block";
+		case FIREBALL:
+			return "Fireball";
+		case FIREWORK:
+			return "Firework";
+		case FISHING_HOOK:
+			return "Fish. Hook";
+		case GHAST:
+			return "Ghast";
+		case GIANT:
+			return "Giant";
+		case GUARDIAN:
+			return "Guardian";
+		case HORSE:
+			return "Horse";
+		case IRON_GOLEM:
+			return "Iron Golem";
+		case ITEM_FRAME:
+			return "Item Frame";
+		case LEASH_HITCH:
+			return "Leash";
+		case LIGHTNING:
+			return "Lightning";
+		case MAGMA_CUBE:
+			return "Magma Cube";
+		case MINECART:
+			return "Minecart";
+		case MINECART_CHEST:
+			return "Minecart Chest";
+		case MINECART_COMMAND:
+			return "Minecart CommandBlock";
+		case MINECART_FURNACE:
+			return "Minecart Furnace";
+		case MINECART_HOPPER:
+			return "Minecart Hopper";
+		case MINECART_MOB_SPAWNER:
+			return "Minecart Mob Spawner";
+		case MINECART_TNT:
+			return "Minecart";
+		case MUSHROOM_COW:
+			return "Mushroom Cow";
+		case OCELOT:
+			return "Ocelot";
+		case PAINTING:
+			return "Painting";
+		case PIG:
+			return "Pig";
+		case PIG_ZOMBIE:
+			return "Zombie Pigman";
+		case PLAYER:
+			return "Player";
+		case PRIMED_TNT:
+			return "TNT";
+		case RABBIT:
+			return "Rabbit";
+		case SHEEP:
+			return "Sheep";
+		case SILVERFISH:
+			return "Silverfish";
+		case SKELETON:
+			return "Skeleton";
+		case SLIME:
+			return "Slime";
+		case SMALL_FIREBALL:
+			return "Fireball";
+		case SNOWBALL:
+			return "Snowball";
+		case SNOWMAN:
+			return "Snow Golem";
+		case SPIDER:
+			return "Spider";
+		case SPLASH_POTION:
+			return "Potion";
+		case SQUID:
+			return "Squid";
+		case THROWN_EXP_BOTTLE:
+			return "Thrown Exp. Bottle";
+		case VILLAGER:
+			return "Villager";
+		case WITCH:
+			return "Witch";
+		case WITHER:
+			return "Wither";
+		case WITHER_SKULL:
+			return "Wither";
+		case WOLF:
+			return "Wolf";
+		case ZOMBIE:
+			return "Zombie";
+		default:
+			return "?";
+		}
+	}
+	
 	/**
 	* Converts an {@link org.bukkit.inventory.ItemStack} to a Json string
 	* for sending with {@link net.md_5.bungee.api.chat.BaseComponent}'s.
@@ -123,33 +257,31 @@ public class NameUtils {
 	    return itemAsJsonObject.toString();
 	}
 	
-	public static String[] parseLine(String line)
-	  {
+	public static String[] parseLine(String line) {
 	    List<String> fields = new ArrayList<String>();
 	    StringBuilder sb = new StringBuilder();
-	    for (int i = 0; i < line.length(); i++)
-	    {
-	      char c = line.charAt(i);
-	      if (c == ',')
-	      {
-	        fields.add(sb.toString());
-	        sb = new StringBuilder();
-	      }
-	      else if (c == '"')
-	      {
-	        int ends = line.indexOf('"', i + 1);
-	        if (ends == -1) {
-	          throw new IllegalArgumentException("Expected double-quote to terminate (" + i + "): " + line);
-	        }
-	        sb.append(line.substring(i + 1, ends - 1));
-	        i = ends;
-	      }
-	      else
-	      {
-	        sb.append(c);
-	      }
+	    
+	    for (int i = 0; i < line.length(); i++) {
+	    	char c = line.charAt(i);
+	    	if (c == ',') {
+	    		fields.add(sb.toString());
+	    		sb = new StringBuilder();
+	    	}
+	    	else if (c == '"') {
+	    		int ends = line.indexOf('"', i + 1);
+	    		
+	    		if (ends == -1) {
+	    			throw new IllegalArgumentException("Expected double-quote to terminate (" + i + "): " + line);
+	    		}
+	        
+	    		sb.append(line.substring(i + 1, ends - 1));
+	    		i = ends;
+	    	} 
+	    	else {
+	    		sb.append(c);
+	    	}
 	    }
 	    fields.add(sb.toString());
-	    return (String[])fields.toArray(new String[fields.size()]);
-	  }
+	    return fields.toArray(new String[fields.size()]);
+	}
 }
