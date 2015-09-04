79938be966a9f1edf0c9bf677193f0e2c54187f3
LeonTG77 <leontg77@gmail.com>
2015-09-04 11:57:23 +0200

    Added messages if there's no games running.

--- src/com/leontg77/uhc/cmds/MatchpostCommand.java
+++ src/com/leontg77/uhc/cmds/MatchpostCommand.java
@@ -6,12 +6,18 @@ import org.bukkit.command.CommandSender;
 
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Settings;
+import com.leontg77.uhc.utils.GameUtils;
 
 public class MatchpostCommand implements CommandExecutor {	
 	private Settings settings = Settings.getInstance();
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("matchpost")) {
+			if (GameUtils.getTeamSize().startsWith("No") || GameUtils.getTeamSize().startsWith("Open")) {
+				sender.sendMessage(Main.prefix() + "There are no matches running.");
+				return true;
+			}
+			
 			sender.sendMessage(Main.prefix() + "Match post: §a" + settings.getConfig().getString("matchpost"));
 		}
 		return true;
--- src/com/leontg77/uhc/cmds/TimeLeftCommand.java
+++ src/com/leontg77/uhc/cmds/TimeLeftCommand.java
@@ -6,20 +6,27 @@ import org.bukkit.command.CommandSender;
 
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Main.State;
+import com.leontg77.uhc.utils.GameUtils;
 import com.leontg77.uhc.Runnables;
 
 public class TimeLeftCommand implements CommandExecutor {
 
 	public boolean onCommand(CommandSender sender, Command cmd,	String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("timeleft")) {
-			if (State.isState(State.INGAME)) {
-				sender.sendMessage(Main.prefix() + "Time left information:");
-				sender.sendMessage(Runnables.heal <= 0 ? "§8§l» §7Final heal has passed." : "§8§l» §7" + Runnables.heal + " minutes to final heal.");
-				sender.sendMessage(Runnables.pvp <= 0 ? "§8§l» §7PvP is enabled." : "§8§l» §7" + Runnables.pvp + " minutes to pvp.");
-				sender.sendMessage(Runnables.meetup <= 0 ? "§8§l» §7Meetup is now!" : "§8§l» §7" + Runnables.meetup + " minutes to meetup.");
-			} else {
-				sender.sendMessage(Main.prefix() + "The game has not started yet.");
+			if (GameUtils.getTeamSize().startsWith("No") || GameUtils.getTeamSize().startsWith("Open")) {
+				sender.sendMessage(Main.prefix() + "There are no matches running.");
+				return true;
 			}
+			
+			if (!State.isState(State.INGAME)) {
+				sender.sendMessage(Main.prefix() + "The game has not started yet.");
+				return true;
+			}
+			
+			sender.sendMessage(Main.prefix() + "Time left information:");
+			sender.sendMessage(Runnables.heal <= 0 ? "§8§l» §7Final heal has passed." : "§8§l» §7" + Runnables.heal + " minutes to final heal.");
+			sender.sendMessage(Runnables.pvp <= 0 ? "§8§l» §7PvP is enabled." : "§8§l» §7" + Runnables.pvp + " minutes to pvp.");
+			sender.sendMessage(Runnables.meetup <= 0 ? "§8§l» §7Meetup is now!" : "§8§l» §7" + Runnables.meetup + " minutes to meetup.");
 		}
 		return true;
 	}
