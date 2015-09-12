f43b74167bea8a39b203ef47af95aea8ae01b865
LeonTG77 <leontg77@gmail.com>
2015-09-12 15:10:03 +0200

    Bugs \o/

--- src/com/leontg77/uhc/utils/DateUtils.java
+++ src/com/leontg77/uhc/utils/DateUtils.java
@@ -44,7 +44,7 @@ public class DateUtils {
         if (hours > 0) {
             output.append(hours).append('h');
             if (minutes == 0) {
-            	output.append(0).append('m');
+            	output.append(minutes).append('m');
             }
         }
         if (minutes > 0) {
