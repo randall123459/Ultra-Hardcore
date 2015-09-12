2d68cb37a180fe46987a08e9021558ec421bfc29
LeonTG77 <leontg77@gmail.com>
2015-09-12 15:02:21 +0200

    Did some code cleanup

--- src/com/leontg77/uhc/Runnables.java
+++ src/com/leontg77/uhc/Runnables.java
@@ -33,7 +33,13 @@ import com.leontg77.uhc.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class Runnables {
-	public static int task, heal, pvp, meetup;
+	private static Settings settings = Settings.getInstance();
+	private static Game game = Game.getInstance();
+	
+	public static int task;
+	public static int heal;
+	public static int pvp;
+	public static int meetup;
 	
 	/**
 	 * Start the countdown for the game.
@@ -104,7 +110,9 @@ public class Runnables {
 					}
 
 					online.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 250, 100));
-					online.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 6000, 100));
+					if (!online.getUniqueId().toString().equals("cb996bf0-333f-42c2-85ca-3a9bec231cc6")) {
+						online.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 6000, 100));
+					}
 					online.setItemOnCursor(new ItemStack (Material.AIR));
 					online.awardAchievement(Achievement.OPEN_INVENTORY);
 					online.getInventory().setArmorContents(null);
@@ -128,7 +136,7 @@ public class Runnables {
 					online.sendMessage(Main.prefix() + "If you have any questions, use /helpop.");
 					PlayerUtils.sendTitle(online, "§aGo!", "§7Good luck, have fun!", 1, 20, 1);
 					
-					Data data = Data.getFor(online);
+					User data = User.get(online);
 					data.increaseStat("gamesplayed");
 				}
 				
@@ -142,8 +150,9 @@ public class Runnables {
 				for (String e : Scoreboards.getManager().kills.getScoreboard().getEntries()) {
 					Scoreboards.getManager().resetScore(e);
 				}
-				
-				Main.board = false;
+
+				game.setPregameBoard(false);
+				game.setArenaBoard(false);
 				
 				timer();
 				Bukkit.getServer().getPluginManager().registerEvents(new SpecInfo(), Main.plugin);
@@ -151,8 +160,8 @@ public class Runnables {
 				Scoreboards.getManager().setScore("§8» §a§lPvE", 1);
 				Scoreboards.getManager().setScore("§8» §a§lPvE", 0);
 				heal = 1;
-				pvp = Settings.getInstance().getConfig().getInt("time.pvp");
-				meetup = Settings.getInstance().getConfig().getInt("time.meetup");
+				pvp = settings.getConfig().getInt("time.pvp");
+				meetup = settings.getConfig().getInt("time.meetup");
 				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer cancel");
 				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer 60 &cFinal heal in&8:&7");
 				
@@ -192,7 +201,7 @@ public class Runnables {
 						}
 					}
 					
-					if (Main.border == Border.START) {
+					if (game.getBorderShrinkTime() == Border.START) {
 						world.getWorldBorder().setSize(299, meetup * 60);
 						world.setThundering(false);
 						world.setStorm(false);
@@ -234,7 +243,7 @@ public class Runnables {
 				
 				if (heal == -1) {
 					PlayerUtils.broadcast(Main.prefix() + "The chat has been enabled.");
-					Main.muted = false;
+					game.setMuted(false);
 				}
 				
 				if (heal == -2) {
@@ -264,7 +273,7 @@ public class Runnables {
 						
 						world.setPVP(true);
 						
-						if (Main.border == Border.PVP) {
+						if (game.getBorderShrinkTime() == Border.PVP) {
 							world.getWorldBorder().setSize(299, meetup * 60);
 						}
 					}
@@ -279,7 +288,7 @@ public class Runnables {
 					PlayerUtils.broadcast(ChatColor.DARK_GRAY + "»»»»»»»»»»»»»»»«««««««««««««««");
 					PlayerUtils.broadcast(ChatColor.RED + " ");
 					PlayerUtils.broadcast(ChatColor.RED + " Meetup has started!");	
-					if (Main.border == Border.MEETUP) {
+					if (game.getBorderShrinkTime() == Border.MEETUP) {
 						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer 120 &6Meetup is now! &8❘ &cBorder will start shrinking in&8:&7");
 						PlayerUtils.broadcast(ChatColor.RED + " The border will start shrinking in §a2 §cminutes.");	
 					} else {
@@ -309,7 +318,7 @@ public class Runnables {
 					}
 				}
 				
-				if (meetup == -2 && Main.border == Border.MEETUP) {
+				if (meetup == -2 && game.getBorderShrinkTime() == Border.MEETUP) {
 					PlayerUtils.broadcast(Main.prefix() + "Border will now shrink to §6300x300 §7over §a10 §7minutes.");
 					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer cancel");
 					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer 600 &6Meetup is now! &8❘ &cBorder stops shrinking in&8:&7");
@@ -327,7 +336,7 @@ public class Runnables {
 					}
 				}
 				
-				if (meetup == -12 && Main.border == Border.MEETUP) {
+				if (meetup == -12 && game.getBorderShrinkTime() == Border.MEETUP) {
 					PlayerUtils.broadcast(Main.prefix() + "Border has stopped shrinking.");
 					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer cancel");
 					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer -1 &6Meetup is now! &8❘ &cBorder has stopped shrinking.");
@@ -442,257 +451,6 @@ public class Runnables {
 				
 				if (meetup == 1) {
 					PlayerUtils.broadcast(Main.prefix() + "Meetup is in §a1 §7minute.");
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-			}
-		}, 1200, 1200);
-	}
-	
-	/**
-	 * Start the countdown for the Recorded Round.
-	 *
-	 * @deprecated Not in use yet.
-	 */
-	@Deprecated
-	public static void startRR() {
-		for (Player online : PlayerUtils.getPlayers()) {
-			PlayerUtils.sendTitle(online, "§c3", "", 1, 20, 1);
-			online.playSound(online.getLocation(), Sound.SUCCESSFUL_HIT, 1, 0);
-		}
-		
-		Bukkit.getServer().getScheduler().runTaskLater(Main.plugin, new Runnable() {
-			public void run() {
-				for (Player online : PlayerUtils.getPlayers()) {
-					PlayerUtils.sendTitle(online, "§e2", "", 1, 20, 1);
-					online.playSound(online.getLocation(), Sound.SUCCESSFUL_HIT, 1, 0);
-				}
-			}
-		}, 20);
-		
-		Bukkit.getServer().getScheduler().runTaskLater(Main.plugin, new Runnable() {
-			public void run() {
-				for (Player online : PlayerUtils.getPlayers()) {
-					PlayerUtils.sendTitle(online, "§a1", "", 1, 20, 1);
-					online.playSound(online.getLocation(), Sound.SUCCESSFUL_HIT, 1, 0);
-				}
-			}
-		}, 40);
-		
-		Bukkit.getServer().getScheduler().runTaskLater(Main.plugin, new Runnable() {
-			public void run() {
-				for (Player online : PlayerUtils.getPlayers()) {
-					online.playSound(online.getLocation(), Sound.SUCCESSFUL_HIT, 1, 1);
-					
-					if (Spectator.getManager().isSpectating(online)) {
-						for (Achievement a : Achievement.values()) {
-							if (online.hasAchievement(a)) {
-								online.removeAchievement(a);
-							}
-						}
-						
-						for (PotionEffect effect : online.getActivePotionEffects()) {
-							online.removePotionEffect(effect.getType());	
-						}
-						
-						online.awardAchievement(Achievement.OPEN_INVENTORY);
-						online.setHealth(online.getMaxHealth());
-						online.setSaturation(20);
-						online.setFoodLevel(20);
-						online.setFireTicks(0);
-						online.setLevel(0);
-						online.setExp(0);
-						
-						PlayerUtils.sendTitle(online, "§aGo!", "§7Good luck have fun!", 1, 20, 1);
-						continue;
-					}
-					
-					for (Achievement a : Achievement.values()) {
-						if (online.hasAchievement(a)) {
-							online.removeAchievement(a);
-						}
-					}
-					
-					if (online.getGameMode() != GameMode.SURVIVAL) {
-						online.setGameMode(GameMode.SURVIVAL);
-					}
-					
-					for (PotionEffect effect : online.getActivePotionEffects()) {
-						online.removePotionEffect(effect.getType());	
-					}
-
-					online.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 250, 100));
-					online.setItemOnCursor(new ItemStack (Material.AIR));
-					online.awardAchievement(Achievement.OPEN_INVENTORY);
-					online.getInventory().setArmorContents(null);
-					online.setHealth(online.getMaxHealth());
-					online.getInventory().clear();
-					online.setAllowFlight(false);
-					online.setSaturation(20);
-					online.setFoodLevel(20);
-					online.setFlying(false);
-					online.setFireTicks(0);
-					online.setLevel(0);
-					online.setExp(0);
-					
-					Main.kills.put(online.getName(), 0);
-					PlayerUtils.sendTitle(online, "§aGo!", "§7Good luck have fun!", 1, 20, 1);
-				}
-				
-				for (Team team : Teams.getManager().getTeamsWithPlayers()) {
-					Main.teamKills.put(team.getName(), 0);
-					
-					ArrayList<String> players = new ArrayList<String>(team.getEntries());
-					TeamCommand.sTeam.put(team.getName(), players);
-				}
-				
-				rrTimer();
-				State.setState(State.INGAME);
-				pvp = Settings.getInstance().getConfig().getInt("time.pvp");
-				meetup = 0;
-				
-				for (World world : Bukkit.getWorlds()) {
-					if (world.getName().equals("lobby") || world.getName().equals("arena")) {
-						continue;
-					}
-					
-					world.setTime(0);
-					world.setDifficulty(Difficulty.HARD);
-					world.setPVP(false);
-					
-					world.setGameRuleValue("doMobSpawning", "false");
-					world.setGameRuleValue("doDaylightCycle", "true");
-					
-					for (Entity mobs : world.getEntities()) {
-						if (mobs.getType() == EntityType.BLAZE ||
-							mobs.getType() == EntityType.CAVE_SPIDER ||
-							mobs.getType() == EntityType.CREEPER ||
-							mobs.getType() == EntityType.ENDERMAN ||
-							mobs.getType() == EntityType.ZOMBIE ||
-							mobs.getType() == EntityType.WITCH ||
-							mobs.getType() == EntityType.WITHER ||
-							mobs.getType() == EntityType.DROPPED_ITEM ||
-							mobs.getType() == EntityType.GHAST ||
-							mobs.getType() == EntityType.GIANT ||
-							mobs.getType() == EntityType.MAGMA_CUBE ||
-							mobs.getType() == EntityType.DROPPED_ITEM ||
-							mobs.getType() == EntityType.SKELETON ||
-							mobs.getType() == EntityType.SPIDER ||
-							mobs.getType() == EntityType.SLIME ||
-							mobs.getType() == EntityType.SILVERFISH ||
-							mobs.getType() == EntityType.SKELETON || 
-							mobs.getType() == EntityType.EXPERIENCE_ORB) {
-							
-							mobs.remove();
-						}
-					}
-					
-					if (Main.border == Border.START) {
-						world.getWorldBorder().setSize(299, meetup * 60);
-						world.setThundering(false);
-						world.setStorm(false);
-					}
-				}
-			}
-		}, 60);
-	}
-	
-	/**
-	 * Start the Recorded Round timers.
-	 *
-	 * @deprecated Not in use yet.
-	 */
-	@Deprecated
-	public static void rrTimer() {
-		task = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(Main.plugin, new Runnable() {
-			public void run() {
-				pvp--;
-				meetup++;
-
-				if (pvp == 0) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lPvP has been enabled.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 20) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 1.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 40) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 2.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 60) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 3.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 80) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 4.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 100) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 5.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 120) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 6.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 140) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 7.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 160) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 8.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 180) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 9.");
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
-					}
-				}
-				
-				if (meetup == 200) {
-					PlayerUtils.broadcast(Main.prefix() + "§6§lEnd of episode 10.");
-					
 					for (Player online : PlayerUtils.getPlayers()) {
 						online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 0);
 					}
