0bd24c8cb5b677fd96228d616298e658f27c172b
LeonTG77 <leontg77@gmail.com>
2015-09-12 15:10:53 +0200

    Updated boards to save during reloads

--- src/com/leontg77/uhc/cmds/AboardCommand.java
+++ src/com/leontg77/uhc/cmds/AboardCommand.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.scoreboard.DisplaySlot;
 
 import com.leontg77.uhc.Arena;
+import com.leontg77.uhc.Game;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Scoreboards;
 import com.leontg77.uhc.utils.PlayerUtils;
@@ -13,20 +14,22 @@ import com.leontg77.uhc.utils.PlayerUtils;
 public class AboardCommand implements CommandExecutor {	
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Game game = Game.getInstance();
+		
 		if (cmd.getName().equalsIgnoreCase("aboard")) {
 			if (sender.hasPermission("uhc.aboard")) {
-				if (Main.aboard) {
+				if (game.arenaBoard()) {
 					for (String e : Arena.getManager().arenaKills.getScoreboard().getEntries()) {
 						Arena.getManager().resetScore(e);
 					}
 					PlayerUtils.broadcast(Main.prefix() + "Arena board has been disabled.");
 					Scoreboards.getManager().kills.setDisplaySlot(DisplaySlot.SIDEBAR);
-					Main.aboard = false;
+					game.setArenaBoard(false);
 				} else {
 					PlayerUtils.broadcast(Main.prefix() + "Arena board has been enabled.");
 					Arena.getManager().arenaKills.setDisplaySlot(DisplaySlot.SIDEBAR);
-					Main.aboard = true;
-					Main.board = false;
+					game.setPregameBoard(false);
+					game.setArenaBoard(true);
 
 					Arena.getManager().setScore("§8» §a§lPvE", 1);
 					Arena.getManager().setScore("§8» §a§lPvE", 0);
--- src/com/leontg77/uhc/cmds/BoardCommand.java
+++ src/com/leontg77/uhc/cmds/BoardCommand.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import com.leontg77.uhc.Arena;
+import com.leontg77.uhc.Game;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Scoreboards;
 import com.leontg77.uhc.Settings;
@@ -16,24 +17,26 @@ public class BoardCommand implements CommandExecutor {
 	private Settings settings = Settings.getInstance();
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Game game = Game.getInstance();
+		
 		if (cmd.getName().equalsIgnoreCase("board")) {
 			if (sender.hasPermission("uhc.board")) {
-				if (Main.board) {
+				if (game.pregameBoard()) {
 					for (String e : Scoreboards.getManager().kills.getScoreboard().getEntries()) {
 						Scoreboards.getManager().resetScore(e);
 					}
 					
 					PlayerUtils.broadcast(Main.prefix() + "Pregame board disabled.");
-					Main.board = false;
+					game.setPregameBoard(false);
 				} else {
 					for (String e : Scoreboards.getManager().kills.getScoreboard().getEntries()) {
 						Scoreboards.getManager().resetScore(e);
 					}
 					
 					PlayerUtils.broadcast(Main.prefix() + "Pregame board enabled.");
-					Main.board = true;
+					game.setPregameBoard(true);
 
-					if (new TeamCommand().enabled) {
+					if (game.teamManagement()) {
 						Scoreboards.getManager().setScore("§e ", 13);
 						Scoreboards.getManager().setScore("§8» §cTeam:", 12);
 						Scoreboards.getManager().setScore("§8» §7/team", 11);
