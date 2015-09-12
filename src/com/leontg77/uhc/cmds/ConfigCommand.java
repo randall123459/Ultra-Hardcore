9c1f74c6d73e4f5597e245b151a1dadd4f09ea33
LeonTG77 <leontg77@gmail.com>
2015-09-12 15:10:18 +0200

    Updated to new option system

--- src/com/leontg77/uhc/cmds/ConfigCommand.java
+++ src/com/leontg77/uhc/cmds/ConfigCommand.java
@@ -15,15 +15,17 @@ import org.bukkit.entity.Player;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Main.Border;
 import com.leontg77.uhc.Main.State;
+import com.leontg77.uhc.Game;
 import com.leontg77.uhc.Runnables;
 import com.leontg77.uhc.Scoreboards;
 import com.leontg77.uhc.Settings;
 import com.leontg77.uhc.utils.PlayerUtils;
 
 public class ConfigCommand implements CommandExecutor, TabCompleter {
-	private Settings settings = Settings.getInstance();
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, final String[] args) {
+		Game game = Game.getInstance();
+		
 		if (cmd.getName().equalsIgnoreCase("config")) {
 			if (sender.hasPermission("uhc.config")) {
 				if (args.length < 2) {
@@ -50,17 +52,11 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 				switch (type) {
 				case ABSORPTION:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.absorption.enabled", true);
-						settings.saveConfig();
-						Main.absorption = true;
-						
-						PlayerUtils.broadcast(Main.prefix() + "Absoroption has been enabled.");
+						PlayerUtils.broadcast(Main.prefix() + "Absorption has been enabled.");
+						game.setAbsorption(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.absorption.enabled", false);
-						settings.saveConfig();
-						Main.absorption = false;
-
-						PlayerUtils.broadcast(Main.prefix() + "Absoroption has been disabled.");
+						PlayerUtils.broadcast(Main.prefix() + "Absorption has been disabled.");
+						game.setAbsorption(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "Absorption can only be true or false.");
 					}
@@ -75,47 +71,32 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 						return true;
 					}
 					
-					settings.getConfig().set("feature.border.shrinkAt", border.name());
-					settings.saveConfig();
-					
-					Main.border = border;
 					PlayerUtils.broadcast(Main.prefix() + "Border will now shrink " + (border == Border.START ? "from" : "at") + " " + border.name().toLowerCase());
+					game.setBorderShrinkTime(border);
 					break;
 				case DEATHLIGHTNING:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.deathlightning.enabled", true);
-						settings.saveConfig();
-						Main.deathlightning = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "DeathLightning has been enabled.");
+						game.setDeathLightning(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.deathlightning.enabled", false);
-						settings.saveConfig();
-						Main.deathlightning = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "DeathLightning has been disabled.");
+						game.setDeathLightning(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "DeathLightning can only be true or false.");
 					}
 					break;
 				case FFA:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("game.ffa", true);
-						settings.saveConfig();
-						Main.ffa = true;
-						
 						sender.sendMessage(Main.prefix() + "You have enabled ffa mode.");
-
+						game.setFFA(true);
+						
 						for (Player online : PlayerUtils.getPlayers()) {
 							PlayerUtils.setTabList(online);
 						}
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("game.ffa", false);
-						settings.saveConfig();
-						Main.ffa = false;
-
 						sender.sendMessage(Main.prefix() + "You have disabled ffa mode.");
-
+						game.setFFA(false);
+						
 						for (Player online : PlayerUtils.getPlayers()) {
 							PlayerUtils.setTabList(online);
 						}
@@ -133,64 +114,47 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 						return true;
 					}
 					
-					settings.getConfig().set("rates.flint.rate", f);
-					settings.saveConfig();
-
-					Main.flintrate = f;
 					PlayerUtils.broadcast(Main.prefix() + "Flint rates are now §a" + f + "§7.");
+					game.setFlintRates(f);
 					break;
 				case GHASTDROPS:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.ghastdrops.enabled", true);
-						settings.saveConfig();
-						Main.ghastdrops = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "Ghasts now drop gold ingots.");
+						game.setGhastDropGold(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.ghastdrops.enabled", false);
-						settings.saveConfig();
-						Main.ghastdrops = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "Ghasts now drop ghast tears.");
+						game.setGhastDropGold(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "GhastDrops can only be true or false.");
 					}
 					break;
 				case GOLDENHEADS:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.goldenheads.enabled", true);
-						settings.saveConfig();
-						Main.goldenheads = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "GoldenHeads has been enabled.");
+						game.setGoldenHeads(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.goldenheads.enabled", false);
-						settings.saveConfig();
-						Main.goldenheads = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "GoldenHeads has been disabled.");
+						game.setGoldenHeads(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "GoldenHeads can only be true or false.");
 					}
 					break;
 				case HEADHEALS:
-					double heal;
+					int heal;
 					
 					try {
-						heal = Double.parseDouble(args[1]);
+						heal = Integer.parseInt(args[1]);
 					} catch (Exception e) {
 						sender.sendMessage(ChatColor.RED + "Invaild head heal amount.");
 						return true;
 					}
 					
-					settings.getConfig().set("feature.goldenheads.heal", ((int) heal * 2));
-					settings.saveConfig();
-
 					PlayerUtils.broadcast(Main.prefix() + "GoldenHeads now heal §a" + heal + "§7 hearts.");
+					game.setGoldenHeadsHeal(heal);
 					break;
 				case HOST:
-					settings.getConfig().set("game.host", args[1]);
-					settings.saveConfig();
+					Settings.getInstance().getConfig().set("game.host", args[1]);
+					Settings.getInstance().saveConfig();
 					
 					Scoreboards.getManager().kills.setDisplayName("§4UHC §8- §7" + args[1]);
 					sender.sendMessage(Main.prefix() + "You set the host to §a" + args[1] + "§7.");
@@ -200,8 +164,8 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 					}
 					break;
 				case MATCHPOST:
-					settings.getConfig().set("matchpost", args[1]);
-					settings.saveConfig();
+					Settings.getInstance().getConfig().set("matchpost", args[1]);
+					Settings.getInstance().saveConfig();
 					
 					sender.sendMessage(Main.prefix() + "You set the matchpost to §a" + args[1] + "§7.");
 					break;
@@ -215,8 +179,8 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 						return true;
 					}
 					
-					settings.getConfig().set("maxplayers", max);
-					settings.saveConfig();
+					Settings.getInstance().getConfig().set("maxplayers", max);
+					Settings.getInstance().saveConfig();
 
 					PlayerUtils.broadcast(Main.prefix() + "Max player slots are now §a" + max + "§7.");
 					break;
@@ -230,76 +194,52 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 						return true;
 					}
 					
-					settings.getConfig().set("time.meetup", meetup);
-					settings.saveConfig();
+					Settings.getInstance().getConfig().set("time.meetup", meetup);
+					Settings.getInstance().saveConfig();
 
 					Runnables.meetup = meetup;
 					PlayerUtils.broadcast(Main.prefix() + "Meetup is now §a" + meetup + "§7 minutes in.");
 					break;
 				case NERFEDSTRENGTH:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.nerfedStrength.enabled", true);
-						settings.saveConfig();
-						Main.nerfedStrength = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "Strength is now nerfed.");
+						game.setNerfedStrength(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.nerfedStrength.enabled", false);
-						settings.saveConfig();
-						Main.nerfedStrength = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "Strength is no longer nerfed.");
+						game.setNerfedStrength(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "NerfedStrength can only be true or false.");
 					}
 					break;
 				case NETHER:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.nether.enabled", true);
-						settings.saveConfig();
-						Main.nether = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "Nether has been enabled.");
+						game.setNether(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.nether.enabled", false);
-						settings.saveConfig();
-						Main.nether = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "Nether has been disabled.");
+						game.setNether(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "Nether can only be true or false.");
 					}
 					break;
 				case NOTCHAPPLES:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.notchapples.enabled", true);
-						settings.saveConfig();
-						Main.notchapples = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "NotchApples has been enabled.");
+						game.setNotchApples(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.notchapples.enabled", false);
-						settings.saveConfig();
-						Main.notchapples = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "NotchApples has been disabled.");
+						game.setNotchApples(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "NotchApples can only be true or false.");
 					}
 					break;
 				case PEARLDAMAGE:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.pearldamage.enabled", true);
-						settings.saveConfig();
-						Main.pearldamage = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "PearlDamage has been enabled.");
+						game.setPearlDamage(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.pearldamage.enabled", false);
-						settings.saveConfig();
-						Main.pearldamage = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "PearlDamage has been disabled.");
+						game.setPearlDamage(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "PearlDamage can only be true or false.");
 					}
@@ -314,8 +254,8 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 						return true;
 					}
 					
-					settings.getConfig().set("time.pvp", pvp);
-					settings.saveConfig();
+					Settings.getInstance().getConfig().set("time.pvp", pvp);
+					Settings.getInstance().saveConfig();
 
 					Runnables.pvp = pvp;
 					PlayerUtils.broadcast(Main.prefix() + "PvP is now §a" + pvp + "§7 minutes in.");
@@ -327,8 +267,8 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 						bu.append(args[s]).append(" ");
 					}
 					
-					settings.getConfig().set("game.scenarios", ChatColor.translateAlternateColorCodes('&', bu.toString().trim()));
-					settings.saveConfig();
+					Settings.getInstance().getConfig().set("game.scenarios", ChatColor.translateAlternateColorCodes('&', bu.toString().trim()));
+					Settings.getInstance().saveConfig();
 
 					sender.sendMessage(Main.prefix() + "You set the scenarios to §a" + ChatColor.translateAlternateColorCodes('&', bu.toString().trim()) + "§7.");
 
@@ -346,25 +286,16 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 						return true;
 					}
 					
-					settings.getConfig().set("rates.shears.rate", s);
-					settings.saveConfig();
-
-					Main.shearrate = s;
 					PlayerUtils.broadcast(Main.prefix() + "Shear rates are now §a" + s + "§7.");
+					game.setShearRates(s);
 					break;
 				case SHEARS:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("rates.shears.enabled", true);
-						settings.saveConfig();
-						Main.shears = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "Shears has been enabled.");
+						game.setShears(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("rates.shears.enabled", false);
-						settings.saveConfig();
-						Main.shears = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "Shears has been disabled.");
+						game.setShears(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "Shears can only be true or false.");
 					}
@@ -392,11 +323,8 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 						return true;
 					}
 					
-					settings.getConfig().set("game.teamsize", tz);
-					settings.saveConfig();
-
-					Main.teamSize = tz;
 					PlayerUtils.broadcast(Main.prefix() + "The teamsize is now §a" + tz + "§7.");
+					game.setTeamSize(tz);
 					
 					for (Player online : PlayerUtils.getPlayers()) {
 						PlayerUtils.setTabList(online);
@@ -404,17 +332,11 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 					break;
 				case TABCOLORS:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.tabcolors.enabled", true);
-						settings.saveConfig();
-						Main.tabcolors = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "Tab will now have the color of your health.");
+						game.setTabShowsHealthColor(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.tabcolors.enabled", false);
-						settings.saveConfig();
-						Main.tabcolors = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "Tab will no longer have the color of your health.");
+						game.setTabShowsHealthColor(false);
 						
 						for (Player online : PlayerUtils.getPlayers()) {
 							online.setPlayerListName(null);
@@ -425,41 +347,29 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
 					break;
 				case HARDERCRAFTING:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.harderCrafting.enabled", true);
-						settings.saveConfig();
-						Main.harderCrafting = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "HarderCrafting has been enabled.");
+						game.setGoldenMelonNeedsIngots(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.harderCrafting.enabled", false);
-						settings.saveConfig();
-						Main.harderCrafting = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "HarderCrafting has been disabled.");
+						game.setGoldenMelonNeedsIngots(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "TheEnd can only be true or false.");
 					}
 					break;
 				case THEEND:
 					if (args[1].equalsIgnoreCase("true")) {
-						settings.getConfig().set("feature.theend.enabled", true);
-						settings.saveConfig();
-						Main.theend = true;
-						
 						PlayerUtils.broadcast(Main.prefix() + "TheEnd has been enabled.");
+						game.setTheEnd(true);
 					} else if (args[1].equalsIgnoreCase("false")) {
-						settings.getConfig().set("feature.theend.enabled", false);
-						settings.saveConfig();
-						Main.theend = false;
-
 						PlayerUtils.broadcast(Main.prefix() + "TheEnd has been disabled.");
+						game.setTheEnd(false);
 					} else {
 						sender.sendMessage(ChatColor.RED + "TheEnd can only be true or false.");
 					}
 					break;
 				case WORLD:
-					settings.getConfig().set("game.world", args[1]);
-					settings.saveConfig();
+					Settings.getInstance().getConfig().set("game.world", args[1]);
+					Settings.getInstance().saveConfig();
 
 					sender.sendMessage(Main.prefix() + "You set the game world to §a" + args[1] + "§7.");
 					break;
