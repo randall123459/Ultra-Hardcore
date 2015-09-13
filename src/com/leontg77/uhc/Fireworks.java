f24a59b2fe39a59c53e3a4bea79b889af912d173
LeonTG77 <leontg77@gmail.com>
2015-09-13 02:27:25 +0200

    Redid the whole firework class

--- src/com/leontg77/uhc/Fireworks.java
+++ src/com/leontg77/uhc/Fireworks.java
@@ -1,103 +1,97 @@
 package com.leontg77.uhc;
 
-import java.util.ArrayList;
 import java.util.Random;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.Location;
 import org.bukkit.entity.Firework;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import com.leontg77.uhc.utils.BlockUtils;
+
+/**
+ * Firework randomizer class.
+ * <p>
+ * This class contains methods for launching random fireworks, getting a random type and a random color.
+ * 
+ * @author LeonTG77
+ */
 public class Fireworks {
 	private static Fireworks instance = new Fireworks();
-	
-	public static Fireworks getRandomizer() {
+	private Random ran = new Random();
+
+	/**
+	 * Gets the instance of the class.
+	 * 
+	 * @return The instance.
+	 */
+	public static Fireworks getInstance() {
 		return instance;
 	}
- 
-	private Random ran = new Random();
-	
-	private ArrayList<Color> colors = new ArrayList<Color>();
-	private ArrayList<Color> fade = new ArrayList<Color>();
-	private ArrayList<Type> types = new ArrayList<Type>();
- 
-	public void setup() {
-		colors.add(Color.WHITE);
-		colors.add(Color.PURPLE);
-		colors.add(Color.RED);
-		colors.add(Color.GREEN);
-		colors.add(Color.AQUA);
-		colors.add(Color.BLUE);
-		colors.add(Color.FUCHSIA);
-		colors.add(Color.GRAY);
-		colors.add(Color.LIME);
-		colors.add(Color.MAROON);
-		colors.add(Color.YELLOW);
-		colors.add(Color.SILVER);
-		colors.add(Color.TEAL);
-		colors.add(Color.ORANGE);
-		colors.add(Color.OLIVE);
-		colors.add(Color.NAVY);
-		colors.add(Color.BLACK);
-		types.add(Type.BURST);
-		types.add(Type.BALL);
-		types.add(Type.BALL_LARGE);
-		types.add(Type.CREEPER);
-		types.add(Type.STAR);
-		fade.add(Color.WHITE);
-		fade.add(Color.PURPLE);
-		fade.add(Color.RED);
-		fade.add(Color.GREEN);
-		fade.add(Color.AQUA);
-		fade.add(Color.BLUE);
-		fade.add(Color.FUCHSIA);
-		fade.add(Color.GRAY);
-		fade.add(Color.LIME);
-		fade.add(Color.MAROON);
-		fade.add(Color.YELLOW);
-		fade.add(Color.SILVER);
-		fade.add(Color.TEAL);
-		fade.add(Color.ORANGE);
-		fade.add(Color.OLIVE);
-		fade.add(Color.NAVY);
-		fade.add(Color.BLACK);
-	}
- 
-	public Type getRandomType() {
-		return types.get(ran.nextInt(types.size()));
-	}
- 
-	public Color getRandomColor() {
-		return colors.get(ran.nextInt(colors.size()));
-	}
-	 
-	public Color getRandomFade() {		
-		return fade.get(ran.nextInt(fade.size()));
-	}
-	
-	public boolean getRandomFlicker() {
-		return ran.nextBoolean();
-	}
-	
-	public boolean getRandomTrail() {
-		return ran.nextBoolean();
-	}
-	
-	public boolean getFade() {
-		return ran.nextBoolean();
-	}
- 
-	public void launchRandomFirework(Location loc){
+
+	/**
+	 * Launch an random firework at the given location.
+	 * 
+	 * @param loc the location launching at.
+	 */
+	public void launchRandomFirework(Location loc) {
 		Firework item = loc.getWorld().spawn(loc, Firework.class);
 		FireworkMeta meta = item.getFireworkMeta();
-		meta.setPower(1);
+
 		if (ran.nextBoolean()) {
-			meta.addEffect(FireworkEffect.builder().flicker(getRandomFlicker()).trail(getRandomTrail()).with(getRandomType()).withColor(getRandomColor()).build());
+			meta.addEffect(FireworkEffect.builder().flicker(ran.nextBoolean()).trail(ran.nextBoolean()).with(randomType()).withColor(randomColor()).withFade(randomColor()).build());
 		} else {
-			meta.addEffect(FireworkEffect.builder().flicker(getRandomFlicker()).trail(getRandomTrail()).with(getRandomType()).withColor(getRandomColor()).withFade(getRandomFade()).build());
+			meta.addEffect(FireworkEffect.builder().flicker(ran.nextBoolean()).trail(ran.nextBoolean()).with(randomType()).withColor(randomColor()).build());
 		}
+
+		meta.setPower(1);
 		item.setFireworkMeta(meta);
+	}
+	
+	/**
+	 * Launch fireworks around the spawn.
+	 */
+	public void startFireworkShow() {
+		new BukkitRunnable() {
+			int i = 0;
+			
+			public void run() {
+				int x = ran.nextInt(50 * 2) - 50;
+				int z = ran.nextInt(50 * 2) - 50;
+
+				Location loc = new Location(Bukkit.getWorld("lobby"), x + 0.5, 34, z + 0.5);
+				loc.setY(BlockUtils.highestBlock(loc).getY());
+				
+				Fireworks.getInstance().launchRandomFirework(loc.add(0, 1, 0));
+				
+				i++;
+				
+				if (i == 200) {
+					cancel();
+				}
+			}
+		}.runTaskTimer(Main.plugin, 20, 5);
+	}
+	
+	/**
+	 * Gets an random color.
+	 * 
+	 * @return A random color.
+	 */
+	private Color randomColor() {
+		return Color.fromBGR(ran.nextInt(255), ran.nextInt(255), ran.nextInt(255));
+	}
+
+	/**
+	 * Gets an random firework type.
+	 * 
+	 * @return A random firework type.
+	 */
+	private Type randomType() {
+		return Type.values()[ran.nextInt(Type.values().length)];
 	}
 }
