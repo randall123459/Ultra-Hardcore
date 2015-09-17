025a1f1d80e5210a5ab10b32c3e78b45865fedfe
LeonTG77 <leontg77@gmail.com>
2015-09-17 15:41:37 +0200

    Reworked gamemode handling

--- src/com/leontg77/uhc/cmds/GamemodeCommand.java
+++ src/com/leontg77/uhc/cmds/GamemodeCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 
 import com.leontg77.uhc.Main;
 
+@SuppressWarnings("deprecation")
 public class GamemodeCommand implements CommandExecutor {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -22,33 +23,31 @@ public class GamemodeCommand implements CommandExecutor {
 				
 				GameMode mode = null;
 				
-				for (GameMode modes : GameMode.values()) {
-					if (modes.name().startsWith(args[0].toUpperCase())) {
-						mode = modes;
+				try {
+					mode = GameMode.getByValue(Integer.parseInt(args[0]));
+				} 
+				catch (Exception e) {
+					for (GameMode modes : GameMode.values()) {
+						if (modes.name().startsWith(args[0].toUpperCase())) {
+							mode = modes;
+							break;
+						}
 					}
 				}
 				
 				if (mode == null) {
-					if (args[0].equals("0")) {
-						mode = GameMode.SURVIVAL;
-					} else if (args[0].equals("1")) {
-						mode = GameMode.CREATIVE;
-					} else if (args[0].equals("2")) {
-						mode = GameMode.ADVENTURE;
-					} else if (args[0].equals("3")) {
-						mode = GameMode.SPECTATOR;
-					} else {
-						sender.sendMessage(ChatColor.RED + "That is not an vaild gamemode.");
-						return true;
-					}
+					sender.sendMessage(ChatColor.RED + "That is not an vaild gamemode.");
+					return true;
 				}
 				
 				if (args.length == 1) {
 					if (sender instanceof Player) {
 						Player player = (Player) sender;
+						
 						player.setGameMode(mode);
-						player.sendMessage(Main.prefix() + "You have changed your gamemode to §6" + mode.name().toLowerCase() + " §7mode.");
-					} else {
+						player.sendMessage(Main.prefix() + "You are now in §6" + mode.name().toLowerCase() + " §7mode.");
+					} 
+					else {
 						sender.sendMessage(ChatColor.RED + "Only players can change their own gamemode."); 
 					}
 					return true;
@@ -57,9 +56,11 @@ public class GamemodeCommand implements CommandExecutor {
 				if (!sender.hasPermission("uhc.gamemode.other")) {
 					if (sender instanceof Player) {
 						Player player = (Player) sender;
+						
 						player.setGameMode(mode);
-						player.sendMessage(Main.prefix() + "You have changed your gamemode to §6" + mode.name().toLowerCase() + " §7mode.");
-					} else {
+						player.sendMessage(Main.prefix() + "You are now in §6" + mode.name().toLowerCase() + " §7mode.");
+					} 
+					else {
 						sender.sendMessage(ChatColor.RED + "Only players can change their own gamemode."); 
 					}
 					return true;
