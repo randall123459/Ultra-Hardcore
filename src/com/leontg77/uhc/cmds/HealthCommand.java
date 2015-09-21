8c5dca8ce5c00f1d567de120157a9fc93811617a
LeonTG77 <leontg77@gmail.com>
2015-09-21 19:35:57 +0200

    Typing /h without argurments shows your own health

--- src/com/leontg77/uhc/cmds/HealthCommand.java
+++ src/com/leontg77/uhc/cmds/HealthCommand.java
@@ -21,11 +21,20 @@ public class HealthCommand implements CommandExecutor {
 			}
 			
 			if (args.length == 0) {
-				sender.sendMessage(ChatColor.RED + "Usage: /health <player>");
+				if (sender instanceof Player) {
+					Player player = (Player) sender;
+					
+					double health = player.getHealth();
+					double maxhealth = player.getMaxHealth();
+
+					sender.sendMessage(Main.prefix() + "§7You are at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + NumberUtils.makePercent(maxhealth) + "%"));
+				} else {
+					sender.sendMessage(Main.prefix() + "Usage: /health <player>");
+				}
 				return true;
 			}
 			
-			Player target = Bukkit.getPlayer(args[0]);
+			Player target = Bukkit.getServer().getPlayer(args[0]);
 			
 			if (target == null) {
 				sender.sendMessage(ChatColor.RED + "That player is not online.");
@@ -35,7 +44,7 @@ public class HealthCommand implements CommandExecutor {
 			double health = target.getHealth();
 			double maxhealth = target.getMaxHealth();
 
-			sender.sendMessage(Main.prefix() + ChatColor.GOLD + target.getName() + " §7is at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + ((int) NumberUtils.makePercent(maxhealth)) + "%"));
+			sender.sendMessage(Main.prefix() + "§a" + target.getName() + " §7is at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + NumberUtils.makePercent(maxhealth) + "%"));
 		}
 		return true;
 	}
