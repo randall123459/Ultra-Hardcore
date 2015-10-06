69fa55efa71ca41d039678f96c8a5a326300cd05
LeonTG77 <leontg77@gmail.com>
2015-10-06 11:23:50 +0200

    If the player joined after jump is given it will give them and disallowed drinking of milk

--- src/com/leontg77/uhc/scenario/types/TrainingRabbits.java
+++ src/com/leontg77/uhc/scenario/types/TrainingRabbits.java
@@ -2,15 +2,20 @@ package com.leontg77.uhc.scenario.types;
 
 import java.util.HashMap;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import com.leontg77.uhc.Main;
 import com.leontg77.uhc.scenario.Scenario;
 import com.leontg77.uhc.utils.PlayerUtils;
 
@@ -48,11 +53,33 @@ public class TrainingRabbits extends Scenario implements Listener {
 	}
 	
 	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent event) {
-		if (!isEnabled()) {
-			return;
+	public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
+		if (event.getItem().getType() == Material.MILK_BUCKET) {
+			event.getPlayer().sendMessage(Main.prefix() + "You cannot drink milk in TrainingRabbits.");
+			event.setCancelled(true);
+			event.setItem(new ItemStack (Material.AIR));
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+
+		if (jump.containsKey(player.getName())) {
+			jump.put(player.getName(), jump.get(player.getName()) + 1);
+		} else {
+			jump.put(player.getName(), 1);
 		}
 		
+		int level = jump.get(player.getName());
+		
+		if (!player.hasPotionEffect(PotionEffectType.JUMP)) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1726272000, level));
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event) {
 		if (event.getEntity().getKiller() == null) {
 			return;
 		}
@@ -65,7 +92,6 @@ public class TrainingRabbits extends Scenario implements Listener {
 			jump.put(player.getName(), 2);
 		}
 		
-		
 		int level = jump.get(player.getName());
 		
 		player.removePotionEffect(PotionEffectType.JUMP);
@@ -74,7 +100,7 @@ public class TrainingRabbits extends Scenario implements Listener {
 
 	@EventHandler
 	public void onEntityDamage(EntityDamageEvent event) {
-		if (!isEnabled()) {
+		if (!(event.getEntity() instanceof Player)) {
 			return;
 		}
 		
