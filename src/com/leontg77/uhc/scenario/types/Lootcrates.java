4f869537deddd4d7882612514d71a25086347878
LeonTG77 <leontg77@gmail.com>
2015-10-07 15:40:42 +0200

    Made lootcrates be named so you can still craft normal useless chests

--- src/com/leontg77/uhc/scenario/types/Lootcrates.java
+++ src/com/leontg77/uhc/scenario/types/Lootcrates.java
@@ -1,20 +1,19 @@
 package com.leontg77.uhc.scenario.types;
 
+import java.util.Arrays;
 import java.util.Random;
 
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import com.leontg77.uhc.Main;
+import com.leontg77.uhc.Spectator;
 import com.leontg77.uhc.scenario.Scenario;
 import com.leontg77.uhc.utils.PlayerUtils;
 
@@ -38,19 +37,26 @@ public class Lootcrates extends Scenario implements Listener {
 			this.task = new BukkitRunnable() {
 				public void run() {
 					for (Player online : PlayerUtils.getPlayers()) {
+						if (Spectator.getManager().isSpectating(online)) {
+							continue;
+						}
+						
 						int i = new Random().nextInt(2);
 						
-						if (i == 1) {
-							PlayerUtils.giveItem(online, new ItemStack (Material.ENDER_CHEST));
-						} else {
-							PlayerUtils.giveItem(online, new ItemStack (Material.CHEST));
-						}
+						ItemStack item = new ItemStack(i == 0 ? Material.ENDER_CHEST : Material.CHEST);
+						ItemMeta meta = item.getItemMeta();
+						meta.setDisplayName("§bTier " + (i == 0 ? "2" : "1") + " lootcrate");
+						meta.setLore(Arrays.asList("§5§oRight click to open."));
+						item.setItemMeta(meta);
+
+						PlayerUtils.giveItem(online, item);
 					}
+					
 					PlayerUtils.broadcast(Main.prefix() + "Lootcrates has been given out.");
 				}
 			};
-			
-			task.runTaskTimer(Main.plugin, 12000, 12000);
+
+			task.runTaskTimer(Main.plugin, 10800, 12000);
 		} else {
 			task.cancel();
 		}
@@ -58,23 +64,6 @@ public class Lootcrates extends Scenario implements Listener {
 
 	public boolean isEnabled() {
 		return enabled;
-	}
-	
-	@EventHandler
-	public void onBlockPlace(BlockPlaceEvent event) {
-		Player player = event.getPlayer();
-		Block block = event.getBlock();
-
-		if (block.getType() == Material.CHEST) {
-			player.sendMessage(ChatColor.RED + "You cannot place lootcrates.");
-			event.setCancelled(true);
-			return;
-		}
-
-		if (block.getType() == Material.ENDER_CHEST) {
-			player.sendMessage(ChatColor.RED + "You cannot place lootcrates.");
-			event.setCancelled(true);
-		}
 	}
 	
 	@EventHandler
@@ -86,88 +75,76 @@ public class Lootcrates extends Scenario implements Listener {
 		Player player = event.getPlayer();
 		ItemStack item = event.getItem();
 
-		if (item.getType() == Material.CHEST) {
-			int i = new Random().nextInt(5);
-			player.sendMessage(Main.prefix() + "You rolled an §a" + i + "§7.");
-			event.setCancelled(true);
-			
-			switch (i) {
-			case 0:
-				player.setItemInHand(new ItemStack (Material.IRON_PICKAXE));
-				break;
-			case 1:
-				player.setItemInHand(new ItemStack (Material.APPLE, 2));
-				break;
-			case 2:
-				player.setItemInHand(new ItemStack (Material.COOKED_BEEF, 8));
-				break;
-			case 3:
-				player.setItemInHand(new ItemStack (Material.CAKE));
-				break;
-			case 4:
-				player.setItemInHand(new ItemStack (Material.RAW_FISH, 64, (short) 3));
-				break;
-			case 5:
-				player.setItemInHand(new ItemStack (Material.BOW));
-				break;
+		if (item.hasItemMeta() && item.getItemMeta().hasDisplayName()) {
+			if (item.getType() == Material.CHEST && item.getItemMeta().getDisplayName().equals("§bTier 1 lootcrate")) {
+				int i = new Random().nextInt(5);
+				player.sendMessage(Main.prefix() + "You rolled an §a" + (i + 1) + "§7.");
+				event.setCancelled(true);
+				
+				switch (i) {
+				case 0:
+					player.setItemInHand(new ItemStack (Material.IRON_PICKAXE));
+					break;
+				case 1:
+					player.setItemInHand(new ItemStack (Material.APPLE, 2));
+					break;
+				case 2:
+					player.setItemInHand(new ItemStack (Material.COOKED_BEEF, 8));
+					break;
+				case 3:
+					player.setItemInHand(new ItemStack (Material.CAKE));
+					break;
+				case 4:
+					player.setItemInHand(new ItemStack (Material.RAW_FISH, 64, (short) 3));
+					break;
+				case 5:
+					player.setItemInHand(new ItemStack (Material.DIRT, 32));
+					break;
+				}
+				return;
 			}
-			return;
-		}
 
-		if (item.getType() == Material.ENDER_CHEST) {
-			int i = new Random().nextInt(10);
-			player.sendMessage(Main.prefix() + "You rolled an §a" + i + "§7.");
-			event.setCancelled(true);
-			
-			switch (i) {
-			case 0:
-				player.setItemInHand(new ItemStack (Material.DIAMOND));
-				break;
-			case 1:
-				player.setItemInHand(new ItemStack (Material.GOLD_INGOT, 3));
-				break;
-			case 2:
-				player.setItemInHand(new ItemStack (Material.IRON_INGOT, 5));
-				break;
-			case 3:
-				player.setItemInHand(new ItemStack (Material.DIRT, 32));
-				break;
-			case 4:
-				player.setItemInHand(new ItemStack (Material.ENCHANTMENT_TABLE));
-				break;
-			case 5:
-				player.setItemInHand(new ItemStack (Material.DIAMOND_SWORD));
-				break;
-			case 6:
-				player.setItemInHand(new ItemStack (Material.DIAMOND_HELMET));
-				break;
-			case 7:
-				player.setItemInHand(new ItemStack (Material.DIAMOND_BOOTS));
-				break;
-			case 8:
-				player.setItemInHand(new ItemStack (Material.ARROW, 32));
-				break;
-			case 9:
-				player.setItemInHand(new ItemStack (Material.TNT));
-				break;
-			case 10:
-				player.setItemInHand(new ItemStack (Material.FLINT_AND_STEEL));
-				break;
+			if (item.getType() == Material.ENDER_CHEST && item.getItemMeta().getDisplayName().equals("§bTier 2 lootcrate")) {
+				int i = new Random().nextInt(10);
+				player.sendMessage(Main.prefix() + "You rolled an §a" + (i + 1) + "§7.");
+				event.setCancelled(true);
+				
+				switch (i) {
+				case 0:
+					player.setItemInHand(new ItemStack (Material.DIAMOND));
+					break;
+				case 1:
+					player.setItemInHand(new ItemStack (Material.GOLD_INGOT, 3));
+					break;
+				case 2:
+					player.setItemInHand(new ItemStack (Material.IRON_INGOT, 5));
+					break;
+				case 3:
+					player.setItemInHand(new ItemStack (Material.BOW));
+					break;
+				case 4:
+					player.setItemInHand(new ItemStack (Material.ENCHANTMENT_TABLE));
+					break;
+				case 5:
+					player.setItemInHand(new ItemStack (Material.DIAMOND_SWORD));
+					break;
+				case 6:
+					player.setItemInHand(new ItemStack (Material.DIAMOND_HELMET));
+					break;
+				case 7:
+					player.setItemInHand(new ItemStack (Material.DIAMOND_BOOTS));
+					break;
+				case 8:
+					player.setItemInHand(new ItemStack (Material.ARROW, 32));
+					break;
+				case 9:
+					player.setItemInHand(new ItemStack (Material.TNT));
+					break;
+				case 10:
+					player.setItemInHand(new ItemStack (Material.FLINT_AND_STEEL));
+					break;
+				}
 			}
-		}
-	}
-	
-	@EventHandler
-	public void onPrepareItemCraft(PrepareItemCraftEvent event) {
-		ItemStack item = event.getRecipe().getResult();
-		
-		if (item.getType() == Material.CHEST) {
-			event.getInventory().setResult(new ItemStack(Material.AIR));
-			return;
-		}
-		
-		if (item.getType() == Material.ENDER_CHEST) {
-			event.getInventory().setResult(new ItemStack(Material.AIR));
 		}
 	}
 }
