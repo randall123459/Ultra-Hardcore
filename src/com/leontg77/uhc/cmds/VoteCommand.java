8990863bc741fef457a088855e62c16aeab70157
LeonTG77 <leontg77@gmail.com>
2015-10-09 12:49:14 +0200

    Reworked the vote command

--- src/com/leontg77/uhc/cmds/VoteCommand.java
+++ src/com/leontg77/uhc/cmds/VoteCommand.java
@@ -10,18 +10,24 @@ import org.bukkit.command.CommandSender;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * Vote command class
+ * 
+ * @author LeonTG77
+ */
 public class VoteCommand implements CommandExecutor {
 	public static ArrayList<String> voted = new ArrayList<String>();
-	public static boolean running = false;
 	
+	public static boolean running = false;
 	public static int yes = 0;
 	public static int no = 0;
 	
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("vote")) {
 			if (sender.hasPermission("uhc.vote")) {
 				if (args.length == 0) {
-					sender.sendMessage(ChatColor.RED + "Usage: /vote <message|end>");
+					sender.sendMessage(Main.prefix() + "Usage: /vote <message|end>");
 					return true;
 				}
 				
@@ -31,11 +37,12 @@ public class VoteCommand implements CommandExecutor {
 						return true;
 					}
 					
-					PlayerUtils.broadcast(Main.prefix() + "The vote has ended, §a" + yes + " yes §7and §c" + no + " no§7.");
+					PlayerUtils.broadcast(Main.prefix() + "The vote has ended, the results are: §a" + yes + " yes §7and §c" + no + " no§7.");
+					
 					running = false;
+					voted.clear();
 					yes = 0;
 					no = 0;
-					voted.clear();
 					return true;
 				}
 				
@@ -44,7 +51,6 @@ public class VoteCommand implements CommandExecutor {
 					return true;
 				}
 				
-				
 				StringBuilder message = new StringBuilder();
 	               
 		        for (int i = 0; i < args.length; i++) {
@@ -52,13 +58,16 @@ public class VoteCommand implements CommandExecutor {
 		        }
 		        
 		        String msg = message.toString().trim();
+		        
 		        running = true;
 		        voted.clear();
+				yes = 0;
+				no = 0;
 		        
-		        PlayerUtils.broadcast(Main.prefix() + "A vote has started for " + msg + ".");
-		        PlayerUtils.broadcast("§8§l» §7Say §a'y'§7 or §c'n'§7 in chat to vote.");
+		        PlayerUtils.broadcast(Main.prefix() + "A vote has started for: §6" + msg + "§7.");
+		        PlayerUtils.broadcast("§8» §7Say §a'y'§7 or §c'n'§7 in chat to vote.");
 			} else {
-				sender.sendMessage(ChatColor.RED + "You do not have access to that command.");
+				sender.sendMessage(Main.NO_PERMISSION_MESSAGE);
 			}
 		}
 		return true;
