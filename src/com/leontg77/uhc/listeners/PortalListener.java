f9eeb76ebaaeb0cc5cadeea23a5b17fdbeeb3bae
LeonTG77 <leontg77@gmail.com>
2015-10-10 11:37:17 +0200

    With help from ghowden I fixed the entire portal code to look and work better and bug free

--- src/com/leontg77/uhc/listeners/PortalListener.java
+++ src/com/leontg77/uhc/listeners/PortalListener.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 import org.bukkit.TravelAgent;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,11 +18,11 @@ import com.leontg77.uhc.Main;
 import com.leontg77.uhc.utils.LocationUtils;
 
 /**
- * Player listener class.
+ * Portal listener class.
  * <p> 
- * Contains all eventhandlers for player releated events.
+ * Contains all eventhandlers for portal releated events.
  * 
- * @author LeonTG77
+ * @author LeonTG77 with huge help from D4mnX and ghowden
  */
 public class PortalListener implements Listener {
 	private Game game = Game.getInstance();
@@ -32,26 +33,29 @@ public class PortalListener implements Listener {
 		Player player = event.getPlayer();
 		Location from = event.getFrom();
 		
-		if (game.nether() && LocationUtils.hasBlockNearby(Material.PORTAL, event.getFrom())) {
-			String fromName = event.getFrom().getWorld().getName();
+		if (LocationUtils.hasBlockNearby(Material.PORTAL, from)) {
+            if (!game.nether()) {
+            	player.sendMessage(Main.prefix() + "The nether is disabled.");
+            	return;
+            }
+            
+			String fromName = from.getWorld().getName();
 	        String targetName;
 	        
-	        if (event.getFrom().getWorld().getEnvironment().equals(Environment.NETHER)) {
+	        switch (from.getWorld().getEnvironment()) {
+			case NETHER:
 	            if (!fromName.endsWith("_nether")) {
 	            	player.sendMessage(Main.prefix() + "Could not teleport you to the overworld, contact the staff now.");
 	                return;
 	            }
 
 	            targetName = fromName.substring(0, fromName.length() - 7);
-	        } else if (event.getFrom().getWorld().getEnvironment().equals(Environment.NORMAL)) {
-	            if (!LocationUtils.hasBlockNearby(Material.PORTAL, from)) {
-	            	player.sendMessage(Main.prefix() + "Could not teleport you to the nether, contact the staff now.");
-	                return;
-	            }
-
+				break;
+			case NORMAL:
 	            targetName = fromName + "_nether";
-	        } else {
-	            return;
+				break;
+			default:
+				return;
 	        }
 
 	        World world = Bukkit.getServer().getWorld(targetName);
@@ -61,41 +65,38 @@ public class PortalListener implements Listener {
 	            return;
 	        }
 
-	        Location to = (world.getName().endsWith("_nether") ? new Location(world, (from.getX() / 8), (from.getY() / 8), (from.getZ() / 8), from.getYaw(), from.getPitch()) : new Location(world, (from.getX() * 8), (from.getY() * 8), (from.getZ() * 8), from.getYaw(), from.getPitch()));
-	        
-	        if (LocationUtils.isOutsideOfBorder(to)) {
-	        	
-	        }
+	        double multiplier = from.getWorld().getEnvironment() == Environment.NETHER ? 8D : 0.125D;
+	        Location to = new Location(world, from.getX() * multiplier, from.getY(), from.getZ() * multiplier, from.getYaw(), from.getPitch());
 	        
 	        to = travel.findOrCreate(to);
+	        
+	        to = LocationUtils.findSafeLocationInsideBorder(to, 10, travel);
 
-	        if (to != null) {
-	            event.setTo(to);
-	        } else {
+	        if (to == null || to.getY() < 0) {
             	player.sendMessage(Main.prefix() + "Could not teleport you, contact the staff now.");
+	        } else {
+	            event.setTo(to);
 	        }
-		} else {
-            if (LocationUtils.hasBlockNearby(Material.PORTAL, event.getFrom())) {
-            	player.sendMessage(Main.prefix() + "The nether is disabled.");
-            }
 		}
 		
-		if (game.theEnd() && LocationUtils.hasBlockNearby(Material.ENDER_PORTAL, event.getFrom())) {
-			String fromName = event.getFrom().getWorld().getName();
+		if (LocationUtils.hasBlockNearby(Material.ENDER_PORTAL, from)) {
+            if (!game.theEnd()) {
+            	player.sendMessage(Main.prefix() + "The end is disabled.");
+            	return;
+            }
+            
+			String fromName = from.getWorld().getName();
 	        String targetName;
 	        
-	        if (event.getFrom().getWorld().getEnvironment().equals(Environment.THE_END)) {
+	        switch (from.getWorld().getEnvironment()) {
+			case THE_END:
 	        	event.setTo(Main.getSpawn());
-	            return;
-	        } else if (event.getFrom().getWorld().getEnvironment().equals(Environment.NORMAL)) {
-	            if (!LocationUtils.hasBlockNearby(Material.ENDER_PORTAL, event.getFrom())) {
-	            	player.sendMessage(Main.prefix() + "Could not teleport you to the end, contact the staff now.");
-	                return;
-	            }
-	            
+	        	return;
+			case NORMAL:
 	            targetName = fromName + "_end";
-	        } else {
-	            return;
+				break;
+			default:
+				return;
 	        }
 
 	        World world = Bukkit.getServer().getWorld(targetName);
@@ -110,22 +111,20 @@ public class PortalListener implements Listener {
 			for (int y = to.getBlockY() - 1; y <= to.getBlockY() + 2; y++) {
 				for (int x = to.getBlockX() - 2; x <= to.getBlockX() + 2; x++) {
 					for (int z = to.getBlockZ() - 2; z <= to.getBlockZ() + 2; z++) {
+						Block block = world.getBlockAt(x, y, z);
+						
 						if (y == 48) {
-							to.getWorld().getBlockAt(x, y, z).setType(Material.OBSIDIAN);
-							to.getWorld().getBlockAt(x, y, z).getState().update();
+							block.setType(Material.OBSIDIAN);
+							block.getState().update();
 						} else {
-							to.getWorld().getBlockAt(x, y, z).setType(Material.AIR);
-							to.getWorld().getBlockAt(x, y, z).getState().update();
+							block.setType(Material.AIR);
+							block.getState().update();
 						}
 					}
 				}
 			}
 			
 			event.setTo(to);
-		} else {
-            if (LocationUtils.hasBlockNearby(Material.ENDER_PORTAL, event.getFrom())) {
-            	player.sendMessage(Main.prefix() + "The end is disabled.");
-            }
 		}
     }
 
@@ -134,24 +133,27 @@ public class PortalListener implements Listener {
 		TravelAgent travel = event.getPortalTravelAgent();
 		Location from = event.getFrom();
 		
-		if (game.nether()) {
-			String fromName = event.getFrom().getWorld().getName();
+		if (LocationUtils.hasBlockNearby(Material.PORTAL, from)) {
+            if (!game.nether()) {
+            	return;
+            }
+            
+			String fromName = from.getWorld().getName();
 	        String targetName;
 	        
-	        if (event.getFrom().getWorld().getEnvironment().equals(Environment.NETHER)) {
+	        switch (from.getWorld().getEnvironment()) {
+			case NETHER:
 	            if (!fromName.endsWith("_nether")) {
 	                return;
 	            }
 
 	            targetName = fromName.substring(0, fromName.length() - 7);
-	        } else if (event.getFrom().getWorld().getEnvironment().equals(Environment.NORMAL)) {
-	            if (!LocationUtils.hasBlockNearby(Material.PORTAL, from)) {
-	                return;
-	            }
-
+				break;
+			case NORMAL:
 	            targetName = fromName + "_nether";
-	        } else {
-	            return;
+				break;
+			default:
+				return;
 	        }
 
 	        World world = Bukkit.getServer().getWorld(targetName);
@@ -160,29 +162,35 @@ public class PortalListener implements Listener {
 	            return;
 	        }
 
-	        Location to = (world.getName().endsWith("_nether") ? new Location(world, (from.getX() / 8), (from.getY() / 8), (from.getZ() / 8), from.getYaw(), from.getPitch()) : new Location(world, (from.getX() * 8), (from.getY() * 8), (from.getZ() * 8), from.getYaw(), from.getPitch()));
+	        double multiplier = from.getWorld().getEnvironment() == Environment.NETHER ? 8D : 0.125D;
+	        Location to = new Location(world, from.getX() * multiplier, from.getY(), from.getZ() * multiplier, from.getYaw(), from.getPitch());
+	        
 	        to = travel.findOrCreate(to);
 
-	        if (to != null) {
+	        to = LocationUtils.findSafeLocationInsideBorder(to, 10, travel);
+
+	        if (to != null && to.getY() >= 0) {
 	            event.setTo(to);
 	        }
 		}
 		
-		if (game.theEnd()) {
-			String fromName = event.getFrom().getWorld().getName();
+		if (LocationUtils.hasBlockNearby(Material.ENDER_PORTAL, from)) {
+            if (!game.theEnd()) {
+            	return;
+            }
+            
+			String fromName = from.getWorld().getName();
 	        String targetName;
 	        
-	        if (event.getFrom().getWorld().getEnvironment().equals(Environment.THE_END)) {
+	        switch (from.getWorld().getEnvironment()) {
+			case THE_END:
 	        	event.setTo(Main.getSpawn());
-	            return;
-	        } else if (event.getFrom().getWorld().getEnvironment().equals(Environment.NORMAL)) {
-	            if (!LocationUtils.hasBlockNearby(Material.ENDER_PORTAL, event.getFrom())) {
-	                return;
-	            }
-	            
+	        	return;
+			case NORMAL:
 	            targetName = fromName + "_end";
-	        } else {
-	            return;
+				break;
+			default:
+				return;
 	        }
 
 	        World world = Bukkit.getServer().getWorld(targetName);
@@ -196,12 +204,14 @@ public class PortalListener implements Listener {
 			for (int y = to.getBlockY() - 1; y <= to.getBlockY() + 2; y++) {
 				for (int x = to.getBlockX() - 2; x <= to.getBlockX() + 2; x++) {
 					for (int z = to.getBlockZ() - 2; z <= to.getBlockZ() + 2; z++) {
+						Block block = world.getBlockAt(x, y, z);
+						
 						if (y == 48) {
-							to.getWorld().getBlockAt(x, y, z).setType(Material.OBSIDIAN);
-							to.getWorld().getBlockAt(x, y, z).getState().update();
+							block.setType(Material.OBSIDIAN);
+							block.getState().update();
 						} else {
-							to.getWorld().getBlockAt(x, y, z).setType(Material.AIR);
-							to.getWorld().getBlockAt(x, y, z).getState().update();
+							block.setType(Material.AIR);
+							block.getState().update();
 						}
 					}
 				}
