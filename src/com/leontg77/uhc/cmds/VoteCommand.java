0b5f6746f17a3c34b96f192df5e59063d13e7401
LeonTG77 <leontg77@gmail.com>
2015-10-14 08:51:41 +0200

    Redid the vote command to work better

--- src/com/leontg77/uhc/cmds/VoteCommand.java
+++ src/com/leontg77/uhc/cmds/VoteCommand.java
@@ -24,52 +24,51 @@ public class VoteCommand implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("vote")) {
-			if (sender.hasPermission("uhc.vote")) {
-				if (args.length == 0) {
-					sender.sendMessage(Main.prefix() + "Usage: /vote <message|end>");
-					return true;
-				}
-				
-				if (args[0].equalsIgnoreCase("end")) {
-					if (!running) {
-						sender.sendMessage(ChatColor.RED + "No votes are running.");
-						return true;
-					}
-					
-					PlayerUtils.broadcast(Main.prefix() + "The vote has ended, the results are: §a" + yes + " yes §7and §c" + no + " no§7.");
-					
-					running = false;
-					voted.clear();
-					yes = 0;
-					no = 0;
-					return true;
-				}
-				
-				if (running) {
-					sender.sendMessage(ChatColor.RED + "Theres already a vote running.");
-					return true;
-				}
-				
-				StringBuilder message = new StringBuilder();
-	               
-		        for (int i = 0; i < args.length; i++) {
-		        	message.append(args[i]).append(" ");
-		        }
-		        
-		        String msg = message.toString().trim();
-		        
-		        running = true;
-		        voted.clear();
-				yes = 0;
-				no = 0;
-		        
-		        PlayerUtils.broadcast(Main.prefix() + "A vote has started for: §6" + msg + "§7.");
-		        PlayerUtils.broadcast("§8» §7Say §a'y'§7 or §c'n'§7 in chat to vote.");
-			} else {
-				sender.sendMessage(Main.NO_PERMISSION_MESSAGE);
-			}
+		if (!sender.hasPermission("uhc.vote")) {
+			sender.sendMessage(Main.NO_PERMISSION_MESSAGE);
+			return true;
 		}
+		
+		if (args.length == 0) {
+			sender.sendMessage(Main.PREFIX + "Usage: /vote <message|end>");
+			return true;
+		}
+		
+		if (args[0].equalsIgnoreCase("end")) {
+			if (!running) {
+				sender.sendMessage(ChatColor.RED + "No votes are running.");
+				return true;
+			}
+			
+			PlayerUtils.broadcast(Main.PREFIX + "The vote has ended, the results are: §a" + yes + " yes §7and §c" + no + " no§7.");
+			
+			running = false;
+			voted.clear();
+			yes = 0;
+			no = 0;
+			return true;
+		}
+		
+		if (running) {
+			sender.sendMessage(ChatColor.RED + "Theres already a vote running.");
+			return true;
+		}
+		
+		StringBuilder message = new StringBuilder();
+           
+        for (int i = 0; i < args.length; i++) {
+        	message.append(args[i]).append(" ");
+        }
+        
+        String msg = message.toString().trim();
+        
+        running = true;
+        voted.clear();
+		yes = 0;
+		no = 0;
+        
+        PlayerUtils.broadcast(Main.PREFIX + "A vote has started for: §6" + msg + "§7.");
+        PlayerUtils.broadcast("§8» §7Say §a'y'§7 or §c'n'§7 in chat to vote.");
 		return true;
 	}
 }
