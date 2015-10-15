8b91339bb184039bb0e26f7a5247004af13d3af1
LeonTG77 <leontg77@gmail.com>
2015-10-15 17:15:55 +0200

    Made a new prefix for staffchat

--- src/com/leontg77/uhc/cmds/StaffChatCommand.java
+++ src/com/leontg77/uhc/cmds/StaffChatCommand.java
@@ -1,38 +1,40 @@
 package com.leontg77.uhc.cmds;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import com.leontg77.uhc.Main;
-import com.leontg77.uhc.utils.NameUtils;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * StaffChat command class
+ * 
+ * @author LeonTG77
+ */
 public class StaffChatCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("ac")) {
-			if (sender.hasPermission("uhc.staff")) {
-				if (args.length == 0) {
-		    		sender.sendMessage(ChatColor.RED + "Usage: /ac <message>");
-		        	return true;
-		        } 
-		        
-		    	StringBuilder message = new StringBuilder("");
-				
-				for (int i = 0; i < args.length; i++) {
-					message.append(args[i]).append(" ");
-				}
-		               
-		        String msg = message.toString().trim();
-
-				PlayerUtils.broadcast("§c§lStaffChat §8» §a" + (sender instanceof Player ? sender.getName() : NameUtils.fixString(sender.getName(), false)) + "§8: §f" + msg, "uhc.staff");
-			} else {
-				sender.sendMessage(Main.NO_PERMISSION_MESSAGE);
-			}
+		if (!sender.hasPermission("uhc.staff")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
 		}
+		
+		if (args.length == 0) {
+    		sender.sendMessage(Main.PREFIX + "Usage: /ac <message>");
+        	return true;
+        } 
+        
+    	StringBuilder message = new StringBuilder("");
+		
+		for (int i = 0; i < args.length; i++) {
+			message.append(args[i]).append(" ");
+		}
+		
+        String msg = message.toString().trim();
+
+		PlayerUtils.broadcast("§8[§4StaffChat§8] §c" + sender.getName() + "§8 » §f" + msg, "uhc.staff");
 		return true;
 	}
 }
