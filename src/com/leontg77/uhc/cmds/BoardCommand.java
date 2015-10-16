b867d0efde37806e9e7c01c7d356edfe3d0ac951
LeonTG77 <leontg77@gmail.com>
2015-10-16 15:08:55 +0200

    Reworked the /board command to work and look better

--- src/com/leontg77/uhc/cmds/BoardCommand.java
+++ src/com/leontg77/uhc/cmds/BoardCommand.java
@@ -1,6 +1,5 @@
 package com.leontg77.uhc.cmds;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,59 +8,70 @@ import com.leontg77.uhc.Arena;
 import com.leontg77.uhc.Game;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Scoreboards;
-import com.leontg77.uhc.Settings;
 import com.leontg77.uhc.utils.GameUtils;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * Board command class.
+ * 
+ * @author LeonTG77
+ */
 public class BoardCommand implements CommandExecutor {	
-	private Settings settings = Settings.getInstance();
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!sender.hasPermission("uhc.board")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
+		}
+		
+		Scoreboards score = Scoreboards.getInstance();
 		Game game = Game.getInstance();
 		
-		if (cmd.getName().equalsIgnoreCase("board")) {
-			if (sender.hasPermission("uhc.board")) {
-				if (game.pregameBoard()) {
-					for (String e : Scoreboards.getInstance().kills.getScoreboard().getEntries()) {
-						Scoreboards.getInstance().resetScore(e);
-					}
-					
-					PlayerUtils.broadcast(Main.PREFIX + "Pregame board disabled.");
-					game.setPregameBoard(false);
-				} else {
-					for (String e : Scoreboards.getInstance().kills.getScoreboard().getEntries()) {
-						Scoreboards.getInstance().resetScore(e);
-					}
-					
-					PlayerUtils.broadcast(Main.PREFIX + "Pregame board enabled.");
-					game.setPregameBoard(true);
-
-					if (game.teamManagement()) {
-						Scoreboards.getInstance().setScore("§e ", 13);
-						Scoreboards.getInstance().setScore("§8» §cTeam:", 12);
-						Scoreboards.getInstance().setScore("§8» §7/team", 11);
-					}
-					if (Arena.getInstance().isEnabled()) {
-						Scoreboards.getInstance().setScore("§a ", 10);
-						Scoreboards.getInstance().setScore("§8» §cArena:", 9);
-						Scoreboards.getInstance().setScore("§8» §7/a ", 8);
-					}
-					if (!GameUtils.getTeamSize().isEmpty()) {
-						Scoreboards.getInstance().setScore("§b ", 7);
-						Scoreboards.getInstance().setScore("§8» §cTeamsize:", 6);
-						Scoreboards.getInstance().setScore("§8» §7" + GameUtils.getTeamSize(), 5);
-					}
-					Scoreboards.getInstance().setScore("§c ", 4);
-					Scoreboards.getInstance().setScore("§8» §cScenarios:", 3);
-					for (String scen : settings.getConfig().getString("game.scenarios").split(" ")) {
-						Scoreboards.getInstance().setScore("§8» §7" + scen, 2);
-					}
-					Scoreboards.getInstance().setScore("§d ", 1);
-				}
-			} else {
-				sender.sendMessage(Main.PREFIX + ChatColor.RED + "You can't use that command.");
+		if (game.pregameBoard()) {
+			for (String entry : score.board.getEntries()) {
+				score.resetScore(entry);
 			}
+			
+			PlayerUtils.broadcast(Main.PREFIX + "Cleared pregame board.");
+			game.setPregameBoard(false);
+			return true;
 		}
+		
+		for (String entry : score.board.getEntries()) {
+			score.resetScore(entry);
+		}
+		
+		PlayerUtils.broadcast(Main.PREFIX + "Generated pregame board.");
+		game.setPregameBoard(true);
+
+		if (game.teamManagement()) {
+			score.setScore("§e ", 12);
+			score.setScore("§8» §cTeam:", 11);
+			score.setScore("§8» §7/team", 10);
+		}
+		
+		if (Arena.getInstance().isEnabled()) {
+			score.setScore("§a ", 9);
+			score.setScore("§8» §cArena:", 8);
+			score.setScore("§8» §7/a ", 7);
+		}
+		
+		if (!GameUtils.getTeamSize().isEmpty()) {
+			score.setScore("§b ", 6);
+			score.setScore("§8» §cTeamsize:", 5);
+			score.setScore("§8» §7" + GameUtils.getAdvancedTeamSize(), 4);
+		}
+		
+		score.setScore("§c ", 3);
+		score.setScore("§8» §cScenarios:", 2);
+		
+		for (String scen : game.getScenarios().split(" ")) {
+			score.setScore("§8» §7" + scen, 1);
+		}
+		
+		score.setScore("§d ", 1);
+		score.setScore("§d ", 0);
 		return true;
 	}
 }
