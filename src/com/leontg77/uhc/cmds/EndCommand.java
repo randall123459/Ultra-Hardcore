09da053eb46b080743e7ec43106da3b82f92b4ca
LeonTG77 <leontg77@gmail.com>
2015-10-16 15:10:34 +0200

    Reworked the /end command to work and look better

--- src/com/leontg77/uhc/cmds/EndCommand.java
+++ src/com/leontg77/uhc/cmds/EndCommand.java
@@ -1,5 +1,7 @@
 package com.leontg77.uhc.cmds;
 
+import static com.leontg77.uhc.Main.plugin;
+
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,286 +12,213 @@ import java.util.TimeZone;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Team;
 
 import com.leontg77.uhc.Fireworks;
 import com.leontg77.uhc.Game;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Parkour;
-import com.leontg77.uhc.Timers;
 import com.leontg77.uhc.Scoreboards;
 import com.leontg77.uhc.Settings;
 import com.leontg77.uhc.Spectator;
 import com.leontg77.uhc.Spectator.SpecInfo;
-import com.leontg77.uhc.User.Stat;
 import com.leontg77.uhc.State;
 import com.leontg77.uhc.Teams;
+import com.leontg77.uhc.Timers;
 import com.leontg77.uhc.User;
+import com.leontg77.uhc.User.Stat;
 import com.leontg77.uhc.scenario.Scenario;
 import com.leontg77.uhc.scenario.ScenarioManager;
 import com.leontg77.uhc.utils.GameUtils;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * End command class.
+ * 
+ * @author LeonTG77
+ */
 public class EndCommand implements CommandExecutor {
 	private Settings settings = Settings.getInstance();
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd,	String label, String[] args) {
-		Game game = Game.getInstance();
+		if (!sender.hasPermission("uhc.end")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
+		}
 		
-		if (cmd.getName().equalsIgnoreCase("end")) {
-			if (sender.hasPermission("uhc.end")) {
-				if (game.isRecordedRound()) {
-					HandlerList.unregisterAll(new SpecInfo());
-					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer cancel");
-					Spectator.getManager().spectators.clear();
-					SpecInfo.totalDiamonds.clear();
-					Parkour.getInstance().setup();
-					State.setState(State.LOBBY);
-					SpecInfo.totalGold.clear();
-					
-					World w = Bukkit.getServer().getWorld(settings.getData().getString("spawn.world"));
-					double x = settings.getData().getDouble("spawn.x");
-					double y = settings.getData().getDouble("spawn.y");
-					double z = settings.getData().getDouble("spawn.z");
-					float yaw = (float) settings.getData().getDouble("spawn.yaw");
-					float pitch = (float) settings.getData().getDouble("spawn.pitch");
+		if (args.length < 2) {
+			sender.sendMessage(Main.PREFIX + "Usage: /end <kills> <winners>");
+			return true;
+		}
+		
+		int kills;
+		
+		try {
+			kills = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not a vaild number.");
+			return true;
+		}
+		
+		ArrayList<String> winners = new ArrayList<String>();
+		
+		PlayerUtils.broadcast(Main.PREFIX + "The game is now over!");
+		PlayerUtils.broadcast(" ");
+		PlayerUtils.broadcast(Main.PREFIX + "The winners are:");
+		
+		for (int i = 1; i < args.length; i++) {
+			OfflinePlayer winner = PlayerUtils.getOfflinePlayer(args[i]);
+			
+			User data = User.get(winner);
+			data.increaseStat(Stat.WINS);
+			
+			PlayerUtils.broadcast("§8» §7" + args[i]);
+			winners.add(args[i]);
+		}
+		
+		PlayerUtils.broadcast(" ");
+		PlayerUtils.broadcast(Main.PREFIX + "With §a" + kills + "§7 kills.");
+		PlayerUtils.broadcast(Main.PREFIX + "View the hall of fame with §a/hof");
+		PlayerUtils.broadcast(" ");
+		PlayerUtils.broadcast(Main.PREFIX + "Congrats on the win and thanks for playing!");
+		
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		String host = GameUtils.getCurrentHost();
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = new Date();
+		
+		int matchcount = 1;
+		
+		if (settings.getHOF().contains(host)) {
+			matchcount = settings.getHOF().getConfigurationSection(host).getKeys(false).size() + 1;
+		}
 
-					Location loc = new Location(w, x, y, z, yaw, pitch);
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						for (Player online2 : PlayerUtils.getPlayers()) {
-							online.showPlayer(online2);
-							online2.showPlayer(online);
-						}
-						
-						if (Spectator.getManager().isSpectating(online)) {
-							Spectator.getManager().disableSpecmode(online, true);
-						}
-						
-						online.setMaxHealth(20.0);
-						online.setHealth(20.0);
-						online.setFireTicks(0);
-						online.setSaturation(20);
-						online.teleport(loc);
-						online.setLevel(0);
-						online.setExp(0);
-						online.setFoodLevel(20);
-						online.getInventory().clear();
-						online.getInventory().setArmorContents(null);
-						online.setItemOnCursor(new ItemStack (Material.AIR));
-						online.setGameMode(GameMode.SURVIVAL);
-						
-						for (PotionEffect effect : online.getActivePotionEffects()) {
-							online.removePotionEffect(effect.getType());	
-						}
-					}
-					
-					for (String e : Scoreboards.getInstance().kills.getScoreboard().getEntries()) {
-						Scoreboards.getInstance().resetScore(e);
-					}
-					
-					for (String team : Teams.getInstance().getTeam("spec").getEntries()) {
-						Teams.getInstance().getTeam("spec").removeEntry(team);
-					}
+		settings.getHOF().set(host + "." + matchcount + ".date", dateFormat.format(date));
+		settings.getHOF().set(host + "." + matchcount + ".winners", winners);
+		settings.getHOF().set(host + "." + matchcount + ".kills", kills);
+		settings.getHOF().set(host + "." + matchcount + ".teamsize", GameUtils.getTeamSize().trim());
+		settings.getHOF().set(host + "." + matchcount + ".scenarios", settings.getConfig().getString("game.scenarios"));
 
-					for (OfflinePlayer whitelisted : Bukkit.getServer().getWhitelistedPlayers()) {
-	       				whitelisted.setWhitelisted(false);
-	       			}
+		settings.getConfig().set("game.scenarios", "games scheduled");
+		settings.getConfig().set("game.teamsize", 0);
+		settings.getConfig().set("matchpost", "none");
+		settings.getConfig().set("game.ffa", true);
+		
+		settings.saveConfig();
+		settings.saveHOF();
+		
+		for (Scenario scen : ScenarioManager.getInstance().getEnabledScenarios()) {
+			scen.disableScenario();
+		}
 
-					for (Scenario scen : ScenarioManager.getInstance().getEnabledScenarios()) {
-	       				scen.disableScenario();
-	       			}
-					
-					try {
-						Bukkit.getServer().getScheduler().cancelTask(Timers.taskMinutes);;
-					} catch (Exception e) {
-						Bukkit.getLogger().warning("Could not cancel task " + Timers.taskMinutes);
-					}
-					
-					File playerData = new File(Bukkit.getWorlds().get(0).getWorldFolder(), "playerdata");
-					File stats = new File(Bukkit.getWorlds().get(0).getWorldFolder(), "stats");
-					
-					for (File dataFiles : playerData.listFiles()) {
-						dataFiles.delete();
-					}
-					
-					for (File statsFiles : stats.listFiles()) {
-						statsFiles.delete();
-					}
-					return true;
-				}
-				
-				if (args.length < 2) {
-					sender.sendMessage(Main.prefix() + "Usage: /end <kills> <winners>");
-					return true;
-				}
-				
-				int kills;
-				
-				try {
-					kills = Integer.parseInt(args[0]);
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "Invaild kill amount.");
-					return true;
-				}
-				
-				HandlerList.unregisterAll(new SpecInfo());
-				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer cancel");
-				Spectator.getManager().spectators.clear();
-				Fireworks.getInstance().startFireworkShow();
-				SpecInfo.totalDiamonds.clear();
-				Parkour.getInstance().setup();
-				State.setState(State.LOBBY);
-				SpecInfo.totalGold.clear();
-				TeamCommand.sTeam.clear();
-				Main.teamKills.clear();
-				Main.kills.clear();
-				
-				ArrayList<String> winners = new ArrayList<String>();
-				
-				for (int i = 1; i < args.length; i++) {
-					winners.add(args[i]);
-				}
-				
-				World w = Bukkit.getServer().getWorld(settings.getData().getString("spawn.world"));
-				double x = settings.getData().getDouble("spawn.x");
-				double y = settings.getData().getDouble("spawn.y");
-				double z = settings.getData().getDouble("spawn.z");
-				float yaw = (float) settings.getData().getDouble("spawn.yaw");
-				float pitch = (float) settings.getData().getDouble("spawn.pitch");
+		Fireworks firework = Fireworks.getInstance();
+		Parkour parkour = Parkour.getInstance();
+		
+		Spectator spec = Spectator.getInstance();
+		Game game = Game.getInstance();
 
-				Location loc = new Location(w, x, y, z, yaw, pitch);
-				String host = GameUtils.getCurrentHost();
-				
-				PlayerUtils.broadcast(Main.prefix() + "The UHC game has ended, Thanks for playing!");
-				PlayerUtils.broadcast(" ");
-				PlayerUtils.broadcast(Main.prefix() + "The winners are:");
-				
-				for (String entry : winners) {
-					OfflinePlayer m9 = PlayerUtils.getOfflinePlayer(entry);
-					
-					User data = User.get(m9);
-					data.increaseStat(Stat.WINS);
-					PlayerUtils.broadcast("§8» §7" + entry);
-				}
-				
-				PlayerUtils.broadcast(" ");
-				PlayerUtils.broadcast(Main.prefix() + "With §a" + kills + "§7 kills.");
-				PlayerUtils.broadcast(Main.prefix() + "Congrats on the win!");
-				PlayerUtils.broadcast(Main.prefix() + "View the hall of fame with §a/hof");
-				
-				TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-				DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-				Date date = new Date();
-				
-				if (settings.getHOF().contains(host)) {
-					int id = settings.getHOF().getConfigurationSection(host).getKeys(false).size() + 1;
-
-					settings.getHOF().set(host + "." + id + ".date", dateFormat.format(date));
-					settings.getHOF().set(host + "." + id + ".winners", winners);
-					settings.getHOF().set(host + "." + id + ".kills", kills);
-					settings.getHOF().set(host + "." + id + ".teamsize", GameUtils.getTeamSize().trim());
-					settings.getHOF().set(host + "." + id + ".scenarios", settings.getConfig().getString("game.scenarios"));
-					settings.saveHOF();
-				
-				} else {
-					settings.getHOF().set(host + "." + 1 + ".date", dateFormat.format(date));
-					settings.getHOF().set(host + "." + 1 + ".winners", winners);
-					settings.getHOF().set(host + "." + 1 + ".kills", kills);
-					settings.getHOF().set(host + "." + 1 + ".teamsize", GameUtils.getTeamSize().trim());
-					settings.getHOF().set(host + "." + 1 + ".scenarios", settings.getConfig().getString("game.scenarios"));
-					settings.saveHOF();
-				}
-
-				settings.getConfig().set("game.scenarios", "games scheduled");
-				settings.getConfig().set("game.teamsize", 0);
-				settings.getConfig().set("matchpost", "none");
-				settings.getConfig().set("game.ffa", true);
-				settings.saveConfig();
-
-				Bukkit.getServer().setIdleTimeout(60);
-				game.setTeamSize(0);
-				game.setFFA(true);
-				
-				for (Player online : PlayerUtils.getPlayers()) {
-					for (Player online2 : PlayerUtils.getPlayers()) {
-						online.showPlayer(online2);
-						online2.showPlayer(online);
-					}
-					
-					if (Spectator.getManager().isSpectating(online)) {
-						Spectator.getManager().disableSpecmode(online, true);
-					}
-					
-					online.setMaxHealth(20.0);
-					online.setHealth(20.0);
-					online.setFireTicks(0);
-					online.setSaturation(20);
-					online.teleport(loc);
-					online.setLevel(0);
-					online.setExp(0);
-					online.setFoodLevel(20);
-					online.getInventory().clear();
-					online.getInventory().setArmorContents(null);
-					online.setItemOnCursor(new ItemStack (Material.AIR));
-					online.setGameMode(GameMode.SURVIVAL);
-					
-					for (PotionEffect effect : online.getActivePotionEffects()) {
-						online.removePotionEffect(effect.getType());	
-					}
-				}
-				
-				for (String e : Scoreboards.getInstance().kills.getScoreboard().getEntries()) {
-					Scoreboards.getInstance().resetScore(e);
-				}
-				
-				for (String team : Teams.getInstance().getTeam("spec").getEntries()) {
-					Teams.getInstance().getTeam("spec").removeEntry(team);
-				}
-
-				for (OfflinePlayer whitelisted : Bukkit.getServer().getWhitelistedPlayers()) {
-       				whitelisted.setWhitelisted(false);
-       			}
-
-				for (Scenario scen : ScenarioManager.getInstance().getEnabledScenarios()) {
-       				scen.disableScenario();
-       			}
-				
-				try {
-					Bukkit.getServer().getScheduler().cancelTask(Timers.taskMinutes);
-				} catch (Exception e) {
-					Bukkit.getLogger().warning("Could not cancel task " + Timers.taskMinutes);
-				}
-				
-				try {
-					Bukkit.getServer().getScheduler().cancelTask(Timers.taskSeconds);
-				} catch (Exception e) {
-					Bukkit.getLogger().warning("Could not cancel task " + Timers.taskSeconds);
-				}
-				
-				File playerData = new File(Bukkit.getWorlds().get(0).getWorldFolder(), "playerdata");
-				File stats = new File(Bukkit.getWorlds().get(0).getWorldFolder(), "stats");
-				
-				for (File dataFiles : playerData.listFiles()) {
-					dataFiles.delete();
-				}
-				
-				for (File statsFiles : stats.listFiles()) {
-					statsFiles.delete();
-				}
-			} else {
-				sender.sendMessage(ChatColor.RED + "You do not have access to that command.");
+		for (Player online : PlayerUtils.getPlayers()) {
+			for (Player onlineTwo : PlayerUtils.getPlayers()) {
+				online.showPlayer(onlineTwo);
+				onlineTwo.showPlayer(online);
 			}
+			
+			if (spec.isSpectating(online)) {
+				spec.disableSpecmode(online, true);
+			}
+
+			online.setGameMode(GameMode.SURVIVAL);
+			online.teleport(Main.getSpawn());
+			online.setMaxHealth(20.0);
+			online.setFireTicks(0);
+			
+			User user = User.get(online);
+			user.reset();
+		}
+		
+		HandlerList.unregisterAll(new SpecInfo());
+		State.setState(State.LOBBY);
+
+		firework.startFireworkShow();
+		parkour.setup();
+		
+		spec.spectators.clear();
+		TeamCommand.sTeam.clear();
+		
+		SpecInfo.totalDiamonds.clear();
+		SpecInfo.totalGold.clear();
+		
+		Main.teamKills.clear();
+		Main.kills.clear();
+
+		Bukkit.getServer().setIdleTimeout(60);
+		Main.plugin.saveData();
+		
+		game.setTeamSize(0);
+		game.setFFA(true);
+		
+		Teams teams = Teams.getInstance();
+		Team team = teams.getTeam("spec");
+		
+		for (String member : team.getEntries()) {
+			team.removeEntry(member);
+		}
+
+		for (OfflinePlayer whitelisted : Bukkit.getWhitelistedPlayers()) {
+			whitelisted.setWhitelisted(false);
+		}
+
+		new BukkitRunnable() {
+			public void run() {
+				Scoreboards board = Scoreboards.getInstance();
+				
+				for (String entry : board.board.getEntries()) {
+					Scoreboards.getInstance().resetScore(entry);
+				}
+				
+				Teams teams = Teams.getInstance();
+				
+				for (Team team : teams.getTeams()) {
+					for (String member : team.getEntries()) {
+						team.removeEntry(member);
+					}
+				}
+				
+				PlayerUtils.broadcast(Main.PREFIX + "Reset scoreboards and teams.");
+				
+			}
+		}.runTaskLater(Main.plugin, 600);
+		
+		File playerData = new File(Bukkit.getWorlds().get(0).getWorldFolder(), "playerdata");
+		File stats = new File(Bukkit.getWorlds().get(0).getWorldFolder(), "stats");
+		
+		for (File dataFiles : playerData.listFiles()) {
+			dataFiles.delete();
+		}
+		
+		for (File statsFiles : stats.listFiles()) {
+			statsFiles.delete();
+		}
+		
+		try {
+			Bukkit.getServer().getScheduler().cancelTask(Timers.taskMinutes);
+		} catch (Exception e) {
+			plugin.getLogger().warning("Could not cancel task " + Timers.taskMinutes);
+		}
+		
+		try {
+			Bukkit.getServer().getScheduler().cancelTask(Timers.taskSeconds);
+		} catch (Exception e) {
+			plugin.getLogger().warning("Could not cancel task " + Timers.taskSeconds);
 		}
 		return true;
 	}
