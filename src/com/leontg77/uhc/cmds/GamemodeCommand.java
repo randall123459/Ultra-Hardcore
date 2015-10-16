a67d791680fce6082c9744ae3b4a950bf0465802
LeonTG77 <leontg77@gmail.com>
2015-10-16 15:10:49 +0200

    Reworked the /gamemode command to work and look better

--- src/com/leontg77/uhc/cmds/GamemodeCommand.java
+++ src/com/leontg77/uhc/cmds/GamemodeCommand.java
@@ -10,76 +10,73 @@ import org.bukkit.entity.Player;
 
 import com.leontg77.uhc.Main;
 
+/**
+ * Gamemode command class.
+ * 
+ * @author LeonTG77
+ */
 @SuppressWarnings("deprecation")
 public class GamemodeCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("gamemode")) {
-			if (sender.hasPermission("uhc.gamemode")) {
-				if (args.length == 0) {
-					sender.sendMessage(ChatColor.RED + "Usage: /gamemode <mode> [player]");
-					return true;
+		if (!sender.hasPermission("uhc.gamemode")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
+		}
+		
+		if (args.length == 0) {
+			sender.sendMessage(Main.PREFIX + "Usage: /gamemode <mode> [player]");
+			return true;
+		}
+		
+		GameMode mode = null;
+		
+		try {
+			mode = GameMode.getByValue(Integer.parseInt(args[0]));
+		} 
+		catch (Exception e) {
+			for (GameMode modes : GameMode.values()) {
+				if (modes.name().startsWith(args[0].toUpperCase())) {
+					mode = modes;
+					break;
 				}
-				
-				GameMode mode = null;
-				
-				try {
-					mode = GameMode.getByValue(Integer.parseInt(args[0]));
-				} 
-				catch (Exception e) {
-					for (GameMode modes : GameMode.values()) {
-						if (modes.name().startsWith(args[0].toUpperCase())) {
-							mode = modes;
-							break;
-						}
-					}
-				}
-				
-				if (mode == null) {
-					sender.sendMessage(ChatColor.RED + "That is not an vaild gamemode.");
-					return true;
-				}
-				
-				if (args.length == 1) {
-					if (sender instanceof Player) {
-						Player player = (Player) sender;
-						
-						player.setGameMode(mode);
-						player.sendMessage(Main.prefix() + "You are now in §6" + mode.name().toLowerCase() + " §7mode.");
-					} 
-					else {
-						sender.sendMessage(ChatColor.RED + "Only players can change their own gamemode."); 
-					}
-					return true;
-				}
-				
-				if (!sender.hasPermission("uhc.gamemode.other")) {
-					if (sender instanceof Player) {
-						Player player = (Player) sender;
-						
-						player.setGameMode(mode);
-						player.sendMessage(Main.prefix() + "You are now in §6" + mode.name().toLowerCase() + " §7mode.");
-					} 
-					else {
-						sender.sendMessage(ChatColor.RED + "Only players can change their own gamemode."); 
-					}
-					return true;
-				}
-				
-				Player target = Bukkit.getServer().getPlayer(args[1]);
-				
-				if (target == null) {
-					sender.sendMessage(ChatColor.RED + "That player is not online.");
-					return true;
-				}
-				
-				target.setGameMode(mode);
-				target.sendMessage(Main.prefix() + ChatColor.GOLD + sender.getName() + " §7has changed your gamemode to §6" + mode.name().toLowerCase() + " §7mode.");
-				sender.sendMessage(Main.prefix() + "You have changed §6" + target.getName() + "'s §7gamemode to §6" + mode.name().toLowerCase() + " §7mode.");
-			} else {
-				sender.sendMessage(ChatColor.RED + "You do not have access to that command.");
 			}
 		}
+		
+		if (mode == null) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not a vaild gamemode.");
+			return true;
+		}
+		
+		if (args.length == 1) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Only players can change their own gamemode."); 
+				return true;
+			}
+			
+			Player player = (Player) sender;
+			
+			player.sendMessage(Main.PREFIX + "You are now in §6" + mode.name().toLowerCase() + " §7mode.");
+			player.setGameMode(mode);
+			return true;
+		}
+		
+		if (!sender.hasPermission("uhc.gamemode.other")) {
+			sender.sendMessage(Main.PREFIX + "§cYou cannot change the gamemode of other players.");
+			return true;
+		}
+		
+		Player target = Bukkit.getServer().getPlayer(args[1]);
+		
+		if (target == null) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not online.");
+			return true;
+		}
+
+		sender.sendMessage(Main.PREFIX + "You have changed §a" + target.getName() + "'s §7gamemode to §6" + mode.name().toLowerCase() + " §7mode.");
+		target.sendMessage(Main.PREFIX + "You are now in §6" + mode.name().toLowerCase() + " §7mode.");
+		target.setGameMode(mode);
 		return true;
 	}
 }
