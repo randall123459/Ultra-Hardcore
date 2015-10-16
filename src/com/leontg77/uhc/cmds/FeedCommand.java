f48cfd936cde6753819603e617868c956f015d0a
LeonTG77 <leontg77@gmail.com>
2015-10-16 15:10:41 +0200

    Reworked the /feed command to work and look better

--- src/com/leontg77/uhc/cmds/FeedCommand.java
+++ src/com/leontg77/uhc/cmds/FeedCommand.java
@@ -8,59 +8,62 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.leontg77.uhc.Main;
+import com.leontg77.uhc.User;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * Feed command class.
+ * 
+ * @author LeonTG77
+ */
 public class FeedCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, final String[] args) {
-		if (cmd.getName().equalsIgnoreCase("feed")) {
-			if (sender.hasPermission("uhc.feed")) {
-				if (args.length == 0) {
-					if (sender instanceof Player) {
-						Player player = (Player) sender;
-						player.setFoodLevel(20);
-						player.setSaturation(20.0f);
-						player.sendMessage(Main.prefix() + "You have been fed.");
-					} else {
-						sender.sendMessage(ChatColor.RED + "Only players can feed themselves.");
-					}
-					return true;
-				}
-				
-				if (sender.hasPermission("uhc.feed.other")) {
-					if (args[0].equals("*")) {
-						for (Player online : PlayerUtils.getPlayers()) {
-							online.setFoodLevel(20);
-							online.setSaturation(20.0f);
-						}
-						PlayerUtils.broadcast(Main.prefix() + "All players have been fed.");
-					} 
-					else {
-						Player target = Bukkit.getServer().getPlayer(args[0]);
-						
-						if (target == null) {
-							sender.sendMessage(ChatColor.RED + "That player is not online.");
-						}
-
-						target.setFoodLevel(20);
-						target.setSaturation(20.0f);
-						target.sendMessage(Main.prefix() + "You have been fed.");
-						sender.sendMessage(Main.prefix() + "You fed " + target.getName() + ".");
-					}
-				} 
-				else {
-					if (sender instanceof Player) {
-						Player player = (Player) sender;
-						player.setFoodLevel(20);
-						player.setSaturation(20.0f);
-						player.sendMessage(Main.prefix() + "You have been fed.");
-					} else {
-						sender.sendMessage(ChatColor.RED + "Only players can feed themselves.");
-					}
-				}
-			} else {
-				sender.sendMessage(Main.NO_PERM_MSG);
+		if (!sender.hasPermission("uhc.feed")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
+		}
+		
+		if (args.length == 0) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Only players can feed themselves.");
+				return true;
 			}
+			
+			Player player = (Player) sender;
+			User user = User.get(player);
+			
+			player.sendMessage(Main.PREFIX + "You have been fed.");
+			user.resetFood();
+			return true;
+		}
+		
+		if (!sender.hasPermission("uhc.feed.other")) {
+			sender.sendMessage(Main.PREFIX + "§cYou cannot feed other players.");
+			return true;
+		}
+		
+		if (args[0].equals("*")) {
+			for (Player online : PlayerUtils.getPlayers()) {
+				User user = User.get(online);
+				user.resetFood();
+			}
+			
+			PlayerUtils.broadcast(Main.PREFIX + "All players have been fed.");
+		} else {
+			Player target = Bukkit.getServer().getPlayer(args[0]);
+			
+			if (target == null) {
+				sender.sendMessage(ChatColor.RED + args[0] + " is not online.");
+				return true;
+			}
+			
+			User user = User.get(target);
+			user.resetFood();
+
+			sender.sendMessage(Main.PREFIX + "You fed §a" + target.getName() + "§7.");
+			target.sendMessage(Main.PREFIX + "You have been fed.");
 		}
 		return true;
 	}
