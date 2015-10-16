3de0f7051e08cc6d1a7a7238e3714775f244a780
LeonTG77 <leontg77@gmail.com>
2015-10-16 15:10:17 +0200

    Reworked the /clearxp command to work and look better

--- src/com/leontg77/uhc/cmds/ClearXpCommand.java
+++ src/com/leontg77/uhc/cmds/ClearXpCommand.java
@@ -8,59 +8,62 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.leontg77.uhc.Main;
+import com.leontg77.uhc.User;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * ClearXp command class.
+ * 
+ * @author LeonTG77
+ */
 public class ClearXpCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, final String[] args) {
-		if (cmd.getName().equalsIgnoreCase("clearxp")) {
-			if (sender.hasPermission("uhc.clearxp")) {
-				if (args.length == 0) {
-					if (sender instanceof Player) {
-						Player player = (Player) sender;
-						player.setExp(0);
-						player.setLevel(0);
-						player.sendMessage(Main.prefix() + "You cleared your own xp.");
-					} else {
-						sender.sendMessage(ChatColor.RED + "Only players can clear their own xp.");
-					}
-					return true;
-				}
-				
-				if (sender.hasPermission("uhc.clearxp.other")) {
-					if (args[0].equals("*")) {
-						for (Player online : PlayerUtils.getPlayers()) {
-							online.setExp(0);
-							online.setLevel(0);
-						}
-						PlayerUtils.broadcast(Main.prefix() + "All players xp bars have been cleared.");
-					} 
-					else {
-						Player target = Bukkit.getServer().getPlayer(args[0]);
-						
-						if (target == null) {
-							sender.sendMessage(ChatColor.RED + "That player is not online.");
-						}
-
-						target.setExp(0);
-						target.setLevel(0);
-						target.sendMessage(Main.prefix() + "Your xp was cleared.");
-						sender.sendMessage(Main.prefix() + "You cleared " + target.getName() + "'s xp.");
-					}
-				} 
-				else {
-					if (sender instanceof Player) {
-						Player player = (Player) sender;
-						player.setExp(0);
-						player.setLevel(0);
-						player.sendMessage(Main.prefix() + "You cleared your own xp.");
-					} else {
-						sender.sendMessage(ChatColor.RED + "Only players can clear their own xp.");
-					}
-				}
-			} else {
-				sender.sendMessage(Main.NO_PERM_MSG);
+		if (!sender.hasPermission("uhc.clearxp")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
+		}
+		
+		if (args.length == 0) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Only players can clear their own xp level.");
+				return true;
 			}
+			
+			Player player = (Player) sender;
+			User user = User.get(player);
+			
+			player.sendMessage(Main.PREFIX + "You cleared your own xp.");
+			user.resetExp();
+			return true;
+		}
+		
+		if (!sender.hasPermission("uhc.clearxp.other")) {
+			sender.sendMessage(Main.PREFIX + "§cYou cannot clear other players xp levels.");
+			return true;
+		}
+		
+		if (args[0].equals("*")) {
+			for (Player online : PlayerUtils.getPlayers()) {
+				User user = User.get(online);
+				user.resetExp();
+			}
+			
+			PlayerUtils.broadcast(Main.PREFIX + "All players xp levels has been cleared.");
+		} else {
+			Player target = Bukkit.getServer().getPlayer(args[0]);
+			
+			if (target == null) {
+				sender.sendMessage(ChatColor.RED + args[0] + " is not online.");
+				return true;
+			}
+			
+			User user = User.get(target);
+			user.resetExp();
+
+			sender.sendMessage(Main.PREFIX + "You cleared §a" + target.getName() + "'s §7xp level.");
+			target.sendMessage(Main.PREFIX + "Your xp level was cleared.");
 		}
 		return true;
 	}
