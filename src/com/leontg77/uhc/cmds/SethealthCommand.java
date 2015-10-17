8562846d89a396a27a57926b24b8d7f2dd949196
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:27:13 +0200

    Reworked /sethealth to work and look a lot better

--- src/com/leontg77/uhc/cmds/SethealthCommand.java
+++ src/com/leontg77/uhc/cmds/SethealthCommand.java
@@ -8,79 +8,87 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.leontg77.uhc.Main;
+import com.leontg77.uhc.utils.NumberUtils;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * SetHealth command class.
+ * 
+ * @author LeonTG77
+ */
 public class SethealthCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("sethealth")) {
-			if (sender.hasPermission("uhc.sethealth")) {
-				if (args.length == 0) {
-					sender.sendMessage(ChatColor.RED + "Usage: /sethealth <health> [player|*]");
-					return true;
-				}
-				
-				double health;
-				
-				try {
-					health = Double.parseDouble(args[0]);
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "Invaild health.");
-					return true;
-				}
-				
-				if (health < 0) {
-					health = 0;
-				}
-				
-				if (args.length == 1) {
-					if (sender instanceof Player) {
-						Player player = (Player) sender;
-						double hp = health;
-						
-						if (hp > player.getMaxHealth()) {
-							hp = player.getMaxHealth();
-						}
-						
-						player.setHealth(hp);
-						player.sendMessage(Main.prefix() + "You set your own health to §6" + hp);
-					} else {
-						sender.sendMessage(ChatColor.RED + "Only players can change their health.");
-					}
-					return true;
-				}
-				
-				if (args[1].equals("*")) {
-					for (Player online : PlayerUtils.getPlayers()) {
-						double hp = health;
-						
-						if (hp > online.getMaxHealth()) {
-							hp = online.getMaxHealth();
-						}
-						online.setHealth(hp);
-					}
-					PlayerUtils.broadcast(Main.prefix() + "All players health is now §6" + health);
-				} else {
-					Player target = Bukkit.getServer().getPlayer(args[1]);
-					double hp = health;
-					
-					if (target == null) {
-						sender.sendMessage(ChatColor.RED + "That player is not online.");
-						return true;
-					}
-					
-					if (hp > target.getMaxHealth()) {
-						hp = target.getMaxHealth();
-					}
-					
-					target.setHealth(hp);
-					target.sendMessage(Main.prefix() + "Your health was set to §6" + hp);
-					sender.sendMessage(Main.prefix() + "You set §6" + target.getName() + "'s §7health to §6" + hp);
-				}
-			} else {
-				sender.sendMessage(Main.NO_PERM_MSG);
-			}
+		if (!sender.hasPermission("uhc.sethealth")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
 		}
+		
+		if (args.length == 0) {
+			sender.sendMessage(Main.PREFIX + "Usage: /sethealth <health> [player|*]");
+			return true;
+		}
+		
+		double health;
+		
+		try {
+			health = Double.parseDouble(args[0]);
+		} catch (Exception e) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not a vaild health.");
+			return true;
+		}
+		
+		if (health < 0) {
+			health = 0;
+		}
+		
+		if (args.length == 1) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Only players can change their health.");
+				return true;
+			}
+			
+			Player player = (Player) sender;
+			
+			if (health > player.getMaxHealth()) {
+				health = player.getMaxHealth();
+			}
+			
+			player.setHealth(health);
+			player.sendMessage(Main.PREFIX + "You set your health to §6" + NumberUtils.makePercent(health));
+			return true;
+		}
+		
+		if (args[1].equals("*")) {
+			for (Player online : PlayerUtils.getPlayers()) {
+				double h = health;
+				
+				if (h > online.getMaxHealth()) {
+					h = online.getMaxHealth();
+				}
+				
+				online.setHealth(health);
+			}
+			
+			PlayerUtils.broadcast(Main.PREFIX + "All players health was set to §6" + NumberUtils.makePercent(health));
+			return true;
+		}
+		
+		Player target = Bukkit.getServer().getPlayer(args[1]);
+		
+		if (target == null) {
+			sender.sendMessage(ChatColor.RED + "That player is not online.");
+			return true;
+		}
+		
+		if (health > target.getMaxHealth()) {
+			health = target.getMaxHealth();
+		}
+
+		sender.sendMessage(Main.PREFIX + "You set §6" + target.getName() + "'s §7health to §6" + NumberUtils.makePercent(health));
+		target.sendMessage(Main.PREFIX + "Your health was set to §6" + NumberUtils.makePercent(health));
+		target.setHealth(health);
 		return true;
 	}
 }
