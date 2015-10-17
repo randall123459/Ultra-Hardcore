5a5c49d21891424ee15130d3016e9047a6ec906a
LeonTG77 <leontg77@gmail.com>
2015-10-17 12:12:32 +0200

    Reworked /health to work and look better

--- src/com/leontg77/uhc/cmds/HealthCommand.java
+++ src/com/leontg77/uhc/cmds/HealthCommand.java
@@ -14,43 +14,39 @@ import com.leontg77.uhc.utils.NumberUtils;
 public class HealthCommand implements CommandExecutor {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, final String[] args) {
-		if (cmd.getName().equalsIgnoreCase("health")) {
-			if (ScenarioManager.getInstance().getScenario("Paranoia").isEnabled()) {
-				sender.sendMessage(ChatColor.RED + "You cannot view peoples health in paranoia.");
-				return true;
-			}
-			
-			if (ScenarioManager.getInstance().getScenario("TeamHealth").isEnabled()) {
-				sender.sendMessage(ChatColor.RED + "You cannot view peoples health in TeamHealth.");
-				return true;
-			}
-			
-			if (args.length == 0) {
-				if (sender instanceof Player) {
-					Player player = (Player) sender;
-					
-					double health = player.getHealth();
-					double maxhealth = player.getMaxHealth();
-
-					sender.sendMessage(Main.prefix() + "§7You are at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + NumberUtils.makePercent(maxhealth) + "%"));
-				} else {
-					sender.sendMessage(Main.prefix() + "Usage: /health <player>");
-				}
-				return true;
-			}
-			
-			Player target = Bukkit.getServer().getPlayer(args[0]);
-			
-			if (target == null) {
-				sender.sendMessage(ChatColor.RED + "That player is not online.");
-				return true;
-			}
-			
-			double health = target.getHealth();
-			double maxhealth = target.getMaxHealth();
-
-			sender.sendMessage(Main.prefix() + "§a" + target.getName() + " §7is at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + NumberUtils.makePercent(maxhealth) + "%"));
+		ScenarioManager scen = ScenarioManager.getInstance();
+		
+		if (scen.getScenario("Paranoia").isEnabled() || scen.getScenario("TeamHealth").isEnabled()) {
+			sender.sendMessage(ChatColor.RED + "Opps, /h is disabled in this scenario.");
+			return true;
 		}
+		
+		if (args.length == 0) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Only players can view their own health.");
+				return true;
+			}
+			
+			Player player = (Player) sender;
+			
+			double health = player.getHealth();
+			double maxhealth = player.getMaxHealth();
+
+			player.sendMessage(Main.PREFIX + "§7You are at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + NumberUtils.makePercent(maxhealth) + "%"));
+			return true;
+		}
+		
+		Player target = Bukkit.getServer().getPlayer(args[0]);
+		
+		if (target == null) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not online.");
+			return true;
+		}
+		
+		double health = target.getHealth();
+		double maxhealth = target.getMaxHealth();
+
+		sender.sendMessage(Main.PREFIX + "§a" + target.getName() + " §7is at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + NumberUtils.makePercent(maxhealth) + "%"));
 		return true;
 	}
 }
