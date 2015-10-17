5c22f55a7d26e8adbf89084de21c3a10bf3ba4b3
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:26:07 +0200

    Reworked /pvp to work and look a lot better

--- src/com/leontg77/uhc/cmds/PvPCommand.java
+++ src/com/leontg77/uhc/cmds/PvPCommand.java
@@ -15,106 +15,99 @@ import com.leontg77.uhc.Main;
 
 public class PvPCommand implements CommandExecutor, TabCompleter {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("pvp")) {
-			if (sender.hasPermission("uhc.pvp")) {
-				if (args.length < 2) {
-					sender.sendMessage(ChatColor.RED + "Usage: /pvp <world|global> <on|off>");
-					return true;
-				}
-				
-				if (args[0].equalsIgnoreCase("global")) {
-					if (args[1].equalsIgnoreCase("on")) {
-						for (World world : Bukkit.getWorlds()) {
-							world.setPVP(true);
-						}
-						sender.sendMessage(Main.prefix() + "Global pvp has been enabled.");
-					} 
-					else if (args[1].equalsIgnoreCase("off")) {
-						for (World world : Bukkit.getWorlds()) {
-							world.setPVP(false);
-						}
-						sender.sendMessage(Main.prefix() + "Global pvp has been disabled.");
-					}
-					else {
-						sender.sendMessage(ChatColor.RED + "Usage: /pvp <world|global> <on|off>");
-					}
-					return true;
-				}
-				
-				World world = Bukkit.getServer().getWorld(args[0]);
-				
-				if (world == null) {
-					sender.sendMessage(ChatColor.RED + "That world does not exist.");
-					return true;
-				}
-				
-				if (args[1].equalsIgnoreCase("on")) {
-					sender.sendMessage(Main.prefix() + "PvP in §a" + world.getName() + " §7has been enabled.");
+		if (!sender.hasPermission("uhc.pvp")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
+		}
+		
+		if (args.length < 2) {
+			sender.sendMessage(Main.PREFIX + "Usage: /pvp <world|global> <on|off>");
+			return true;
+		}
+		
+		if (args[0].equalsIgnoreCase("global")) {
+			if (args[1].equalsIgnoreCase("on")) {
+				for (World world : Bukkit.getWorlds()) {
 					world.setPVP(true);
-				} 
-				else if (args[1].equalsIgnoreCase("off")) {
-					sender.sendMessage(Main.prefix() + "PvP in §a" + world.getName() + " §7has been disabled.");
+				}
+				sender.sendMessage(Main.PREFIX + "Global pvp has been enabled.");
+			} else if (args[1].equalsIgnoreCase("off")) {
+				for (World world : Bukkit.getWorlds()) {
 					world.setPVP(false);
 				}
-				else {
-					sender.sendMessage(ChatColor.RED + "Usage: /pvp <world|global> <on|off>");
-				}
+				sender.sendMessage(Main.PREFIX + "Global pvp has been disabled.");
 			} else {
-				sender.sendMessage(ChatColor.RED + "You do not have access to that command.");
+				sender.sendMessage(Main.PREFIX + "Usage: /pvp <world|global> <on|off>");
 			}
+			return true;
+		}
+		
+		World world = Bukkit.getServer().getWorld(args[0]);
+		
+		if (world == null) {
+			sender.sendMessage(ChatColor.RED + "That world does not exist.");
+			return true;
+		}
+		
+		if (args[1].equalsIgnoreCase("on")) {
+			sender.sendMessage(Main.PREFIX + "PvP in §a" + world.getName() + " §7has been enabled.");
+			world.setPVP(true);
+		} else if (args[1].equalsIgnoreCase("off")) {
+			sender.sendMessage(Main.PREFIX + "PvP in §a" + world.getName() + " §7has been disabled.");
+			world.setPVP(false);
+		} else {
+			sender.sendMessage(Main.PREFIX + "Usage: /pvp <world|global> <on|off>");
 		}
 		return true;
 	}
 	
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("pvp")) {
-			if (sender.hasPermission("uhc.pvp")) {
-				if (args.length == 1) {
-		        	ArrayList<String> arg = new ArrayList<String>();
-		        	ArrayList<String> types = new ArrayList<String>();
-		        	types.add("global");
-		        	
-		        	for (World world : Bukkit.getServer().getWorlds()) {
-		        		types.add(world.getName());
-		        	}
-		        	
-		        	if (!args[0].equals("")) {
-		        		for (String type : types) {
-		        			if (type.toLowerCase().startsWith(args[0].toLowerCase())) {
-		        				arg.add(type);
-		        			}
-		        		}
-		        	}
-		        	else {
-		        		for (String type : types) {
-	        				arg.add(type);
-		        		}
-		        	}
-		        	return arg;
-		        }
-				else if (args.length == 2) {
-					ArrayList<String> arg = new ArrayList<String>();
-		        	ArrayList<String> types = new ArrayList<String>();
-		        	types.add("on");
-		        	types.add("off");
-		        	
-		        	if (!args[0].equals("")) {
-		        		for (String type : types) {
-		        			if (type.toLowerCase().startsWith(args[0].toLowerCase())) {
-		        				arg.add(type);
-		        			}
-		        		}
-		        	}
-		        	else {
-		        		for (String type : types) {
-	        				arg.add(type);
-		        		}
-		        	}
-		        	return arg;
-		        }
-			}
+		if (!sender.hasPermission("uhc.pvp")) {
+			return null;
 		}
-		return null;
+
+    	ArrayList<String> toReturn = new ArrayList<String>();
+    	
+		if (args.length == 1) {
+        	ArrayList<String> types = new ArrayList<String>();
+        	types.add("global");
+        	
+        	for (World world : Bukkit.getServer().getWorlds()) {
+        		types.add(world.getName());
+        	}
+        	
+        	if (args[0].equals("")) {
+        		for (String type : types) {
+    				toReturn.add(type);
+        		}
+        	} else {
+        		for (String type : types) {
+        			if (type.toLowerCase().startsWith(args[0].toLowerCase())) {
+        				toReturn.add(type);
+        			}
+        		}
+        	}
+        }
+		
+		if (args.length == 2) {
+        	ArrayList<String> types = new ArrayList<String>();
+        	types.add("on");
+        	types.add("off");
+        	
+        	if (args[0].equals("")) {
+        		for (String type : types) {
+        			toReturn.add(type);
+        		}
+        	} else {
+        		for (String type : types) {
+        			if (type.toLowerCase().startsWith(args[0].toLowerCase())) {
+        				toReturn.add(type);
+        			}
+        		}
+        	}
+        }
+		return toReturn;
 	}
 }
