17a3e6a166af879d8a9e74893fbc7f1f5c37a372
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:26:41 +0200

    Reworked /rank to work and look a lot better

--- src/com/leontg77/uhc/cmds/RankCommand.java
+++ src/com/leontg77/uhc/cmds/RankCommand.java
@@ -11,86 +11,90 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-import com.leontg77.uhc.Fireworks;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.User;
 import com.leontg77.uhc.User.Rank;
 import com.leontg77.uhc.utils.NameUtils;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * Rank command class.
+ * 
+ * @author LeonTG77
+ */
 public class RankCommand implements CommandExecutor, TabCompleter {	
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("rank")) {
-			if (sender.hasPermission("uhc.rank")) {
-				if (args.length < 2) {
-					sender.sendMessage(Main.prefix() + "Usage: /rank <player> <newrank>");
-					return true;
-				}
-				
-				Rank rank;
-				
-				try {
-					rank = Rank.valueOf(args[1].toUpperCase());
-				} catch (Exception e) {
-					sender.sendMessage(Main.prefix() + "Invaild rank.");
-					return true;
-				}
-				
-				Player target = Bukkit.getServer().getPlayer(args[0]);
-				OfflinePlayer offline = PlayerUtils.getOfflinePlayer(args[0]);
-				
-				if (target == null) {
-					User.get(offline).setRank(rank);
-					PlayerUtils.broadcast(Main.prefix() + "§6" + offline.getName() + " §7has been given §a" + (rank == Rank.VIP ? "VIP" : NameUtils.fixString(rank.name(), false)) + " §7rank.");
-					return true;
-				}
-				
-				User.get(target).setRank(rank);
-				Fireworks.getInstance().launchRandomFirework(target.getLocation());
-				PlayerUtils.broadcast(Main.prefix() + "§6" + target.getName() + " §7has been given §a" + (rank == Rank.VIP ? "VIP" : NameUtils.fixString(rank.name(), false)) + " §7rank.");
-			} else {
-				sender.sendMessage(Main.prefix() + "You can't use that command.");
-			}
+		if (!sender.hasPermission("uhc.rank")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
 		}
+		
+		if (args.length < 2) {
+			sender.sendMessage(Main.PREFIX + "Usage: /rank <player> <newrank>");
+			return true;
+		}
+		
+		Rank rank;
+		
+		try {
+			rank = Rank.valueOf(args[1].toUpperCase());
+		} catch (Exception e) {
+			sender.sendMessage(Main.PREFIX + args[1] + " is not a vaild rank.");
+			return true;
+		}
+		
+		Player target = Bukkit.getServer().getPlayer(args[0]);
+		OfflinePlayer offline = PlayerUtils.getOfflinePlayer(args[0]);
+		
+		if (target == null) {
+			PlayerUtils.broadcast(Main.PREFIX + "§6" + offline.getName() + " §7has been given §a" + NameUtils.fixString(rank.name(), false) + " §7rank.");
+			User.get(offline).setRank(rank);
+			return true;
+		}
+		
+		PlayerUtils.broadcast(Main.PREFIX + "§6" + target.getName() + " §7has been given §a" + NameUtils.fixString(rank.name(), false) + " §7rank.");
+		User.get(target).setRank(rank);
 		return true;
 	}
 	
+	@Override
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("rank")) {
-			if (sender.hasPermission("uhc.rank")) {
-				ArrayList<String> arg = new ArrayList<String>();
-				
-				if (args.length == 1) {
-					if (!args[0].equals("")) {
-		        		for (Player online : PlayerUtils.getPlayers()) {
-		        			if (online.getName().toLowerCase().startsWith(args[0].toLowerCase())) {
-		        				arg.add(online.getName());
-		        			}
-		        		}
-		        	} else {
-		        		for (Player online : PlayerUtils.getPlayers()) {
-	        				arg.add(online.getName());
-		        		}
-		        	}
-		        }
-				
-				if (args.length == 2) {
-					if (!args[1].equals("")) {
-		        		for (Rank rank : Rank.values()) {
-		        			if (rank.name().toLowerCase().startsWith(args[1].toLowerCase())) {
-		        				arg.add(rank.name().toLowerCase());
-		        			}
-		        		}
-		        	} else {
-		        		for (Rank rank : Rank.values()) {
-	        				arg.add(rank.name().toLowerCase());
-		        		}
-		        	}
-		        }
-				return arg;
-			}
+		if (!sender.hasPermission("uhc.rank")) {
+			return null;
 		}
-		return null;
+		
+		ArrayList<String> toReturn = new ArrayList<String>();
+		
+		if (args.length == 1) {
+			if (args[0].equals("")) {
+        		for (Player online : PlayerUtils.getPlayers()) {
+    				toReturn.add(online.getName());
+        		}
+        	} else {
+        		for (Player online : PlayerUtils.getPlayers()) {
+        			if (online.getName().toLowerCase().startsWith(args[0].toLowerCase())) {
+        				toReturn.add(online.getName());
+        			}
+        		}
+        	}
+        }
+		
+		if (args.length == 2) {
+			if (args[1].equals("")) {
+        		for (Rank rank : Rank.values()) {
+    				toReturn.add(rank.name().toLowerCase());
+        		}
+        	} else {
+        		for (Rank rank : Rank.values()) {
+        			if (rank.name().toLowerCase().startsWith(args[1].toLowerCase())) {
+        				toReturn.add(rank.name().toLowerCase());
+        			}
+        		}
+        	}
+        }
+		
+		return toReturn;
 	}
 }
