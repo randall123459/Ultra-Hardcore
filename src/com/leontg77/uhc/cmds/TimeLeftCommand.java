a2ddff8580c16c0374dd1a8d30cde3f0c58032a9
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:31:56 +0200

    Reworked /timeleft to work and look a lot better

--- src/com/leontg77/uhc/cmds/TimeLeftCommand.java
+++ src/com/leontg77/uhc/cmds/TimeLeftCommand.java
@@ -11,27 +11,33 @@ import com.leontg77.uhc.State;
 import com.leontg77.uhc.utils.DateUtils;
 import com.leontg77.uhc.utils.GameUtils;
 
+/**
+ * Timeleft command class.
+ * 
+ * @author LeonTG77
+ */
 public class TimeLeftCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd,	String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("timeleft")) {
 			if (Game.getInstance().isRecordedRound()) {
-				sender.sendMessage(Main.prefix() + "Current Episode: §a" + Timers.meetup + " mins");
-				sender.sendMessage(Main.prefix() + "Time to next episode: §a" + Timers.heal + " mins");
+				sender.sendMessage(Main.PREFIX + "Current Episode: §a" + Timers.meetup + " mins");
+				sender.sendMessage(Main.PREFIX + "Time to next episode: §a" + Timers.heal + " mins");
 				return true;
 			}
 			
 			if (GameUtils.getTeamSize().startsWith("No") || GameUtils.getTeamSize().startsWith("Open")) {
-				sender.sendMessage(Main.prefix() + "There are no matches running.");
+				sender.sendMessage(Main.PREFIX + "There are no matches running.");
 				return true;
 			}
 			
 			if (!State.isState(State.INGAME)) {
-				sender.sendMessage(Main.prefix() + "The game has not started yet.");
+				sender.sendMessage(Main.PREFIX + "The game has not started yet.");
 				return true;
 			}
 			
-			sender.sendMessage(Main.prefix() + "Timers:");
+			sender.sendMessage(Main.PREFIX + "Timers:");
 			sender.sendMessage(Timers.healSeconds <= 0 ? "§8» §eFinal heal has passed." : "§8» §7Final heal in: §a" + DateUtils.ticksToString(Timers.healSeconds));
 			sender.sendMessage(Timers.pvpSeconds <= 0 ? "§8» §aPvP is enabled." : "§8» §7PvP in: §a" + DateUtils.ticksToString(Timers.pvpSeconds));
 			sender.sendMessage(Timers.meetupSeconds <= 0 ? "§8» §cMeetup is now!" : "§8» §7Meetup in: §a" + DateUtils.ticksToString(Timers.meetupSeconds));
