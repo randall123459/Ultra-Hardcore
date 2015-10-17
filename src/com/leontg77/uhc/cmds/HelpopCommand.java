dfc37d79cf025479123d790c2bca156c3b9d8516
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:25:19 +0200

    Reworked /helpop to work and look a lot better

--- src/com/leontg77/uhc/cmds/HelpopCommand.java
+++ src/com/leontg77/uhc/cmds/HelpopCommand.java
@@ -9,48 +9,61 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.leontg77.uhc.Main;
+import com.leontg77.uhc.Spectator;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * Helpop command class.
+ * 
+ * @author LeonTG77
+ */
 public class HelpopCommand implements CommandExecutor {
 	public static ArrayList<CommandSender> cooldown = new ArrayList<CommandSender>();
 
-	public boolean onCommand(final CommandSender sender, Command cmd, String label, final String[] args) {
-		if (cmd.getName().equalsIgnoreCase("helpop")) {
-			if (args.length == 0) {
-	           	sender.sendMessage(ChatColor.RED + "Usage: /helpop <message>");
-	           	return true;
-			}
-			
-			if (cooldown.contains(sender)) {
-				sender.sendMessage(Main.prefix().replaceAll("UHC", "HelpOp") + "§7Do not spam helpops.");
-				return true;
-			}
-			
-			StringBuilder sb = new StringBuilder("");
-			
-			for (int i = 0; i < args.length; i++) {
-				sb.append(args[i]).append(" ");
-			}
-			
-			String msg = sb.toString().trim();
-
-			PlayerUtils.broadcast(Main.prefix().replaceAll("UHC", "HelpOp") + "§7" + sender.getName() + "§7: §6" + msg, "uhc.staff");
-			for (Player online : PlayerUtils.getPlayers()) {
-				if (online.hasPermission("uhc.staff")) {
-					online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 1);
-				}
-			}
-			cooldown.add(sender);
-			sender.sendMessage(Main.prefix().replaceAll("UHC", "HelpOp") + "§7Helpop sent, please don't spam this.");
-			sender.sendMessage(Main.prefix().replaceAll("UHC", "HelpOp") + "§7Your message: §6" + msg);
-			Bukkit.getServer().getScheduler().runTaskLater(Main.plugin, new Runnable() {
-				public void run() {
-					cooldown.remove(sender);
-				}
-			}, 100);
+	@Override
+	public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args) {
+		if (args.length == 0) {
+           	sender.sendMessage(ChatColor.RED + "Usage: /helpop <message>");
+           	return true;
 		}
+		
+		if (cooldown.contains(sender)) {
+			sender.sendMessage("§4§lHelp§8§l-§4§lOp §8» §7Do not spam helpops.");
+			return true;
+		}
+		
+		Spectator spec = Spectator.getInstance();
+		StringBuilder sb = new StringBuilder("");
+		
+		for (int i = 0; i < args.length; i++) {
+			sb.append(args[i]).append(" ");
+		}
+		
+		String msg = sb.toString().trim();
+
+		for (Player online : PlayerUtils.getPlayers()) {
+			if (!online.hasPermission("uhc.staff") && !spec.isSpectating(online)) {
+				continue;
+			}
+			
+			online.sendMessage("§4§lHelp§8§l-§4§lOp §8» §7" + sender.getName() + "§7: §6" + msg);
+			online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 1);
+		}
+		
+		Bukkit.getLogger().info("§4§lHelp§8§l-§4§lOp §8» §7" + sender.getName() + "§7: §6" + msg);
+		cooldown.add(sender);
+		
+		sender.sendMessage("§4§lHelp§8§l-§4§lOp §8» §7Helpop sent, please don't spam this.");
+		sender.sendMessage("§4§lHelp§8§l-§4§lOp §8» §7Your message: §6" + msg);
+		
+		new BukkitRunnable() {
+			public void run() {
+				cooldown.remove(sender);
+			}
+		}.runTaskLater(Main.plugin, 100);
 		return true;
 	}
 }
