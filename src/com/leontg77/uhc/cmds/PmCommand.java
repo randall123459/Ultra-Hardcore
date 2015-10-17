d2c2300dec97afbddf89b7f31ac51f93e70f9daf
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:26:02 +0200

    Reworked /pm to work and look a lot better

--- src/com/leontg77/uhc/cmds/PmCommand.java
+++ src/com/leontg77/uhc/cmds/PmCommand.java
@@ -1,6 +1,5 @@
 package com.leontg77.uhc.cmds;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,10 +7,18 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
 
+import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Spectator;
+import com.leontg77.uhc.Teams;
 
+/**
+ * Pm command class.
+ * 
+ * @author LeonTG77
+ */
 public class PmCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.RED + "Only players can use teamchat.");
@@ -20,35 +27,30 @@ public class PmCommand implements CommandExecutor {
 		
 		Player player = (Player) sender;
 		
-		if (cmd.getName().equalsIgnoreCase("pm")) {
-			if (args.length == 0) {
-				player.sendMessage(ChatColor.RED + "Usage: /pm <message>");
-				return true;
-			}
-			
-			Team team = player.getScoreboard().getEntryTeam(player.getName());
-			
-			if (team == null) { 
-	        	player.sendMessage(ChatColor.RED + "You are not on a team.");
-	        	return true;
-			} 
-			
-			StringBuilder message = new StringBuilder();
-            
-	        for (int i = 0; i < args.length; i++) {
-	        	message.append(args[i]).append(" ");
-	        }
-	               
-	        String msg = message.toString().trim();
-	        
-			for (String entry : player.getScoreboard().getEntryTeam(player.getName()).getEntries()) {
-				Player teammate = Bukkit.getServer().getPlayer(entry);
-				
-				if (teammate != null) {
-					teammate.sendMessage(Spectator.getInstance().isSpectating(player) ? "§5§lSpecChat §8» §7" + player.getName() + ": §f" + msg : "§9§lTeam §8» §7" + player.getName() + ": §f" + msg);
-				}
-			}
+		if (args.length == 0) {
+			player.sendMessage(Main.PREFIX + "Usage: /pm <message>");
+			return true;
 		}
+		
+		Spectator spec = Spectator.getInstance();
+		Teams teams = Teams.getInstance();
+		
+		Team team = teams.getTeam(player);
+		
+		if (team == null || spec.isSpectating(player)) { 
+        	player.sendMessage(ChatColor.RED + "You are not on a team.");
+        	return true;
+		} 
+		
+		StringBuilder message = new StringBuilder();
+        
+        for (int i = 0; i < args.length; i++) {
+        	message.append(args[i]).append(" ");
+        }
+               
+        String msg = message.toString().trim();
+        
+        teams.sendMessage(team, "§9§lTeam §8» §7" + player.getName() + ": §f" + msg);
 		return true;
 	}
 }
