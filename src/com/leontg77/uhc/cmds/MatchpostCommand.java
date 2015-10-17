5f17749cf0beee09fbdbae832da12627694deb37
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:25:37 +0200

    Reworked /matchpost to work and look a lot better

--- src/com/leontg77/uhc/cmds/MatchpostCommand.java
+++ src/com/leontg77/uhc/cmds/MatchpostCommand.java
@@ -8,18 +8,24 @@ import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Settings;
 import com.leontg77.uhc.utils.GameUtils;
 
+/**
+ * Matchpost command class.
+ * 
+ * @author LeonTG77
+ */
 public class MatchpostCommand implements CommandExecutor {	
-	private Settings settings = Settings.getInstance();
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("matchpost")) {
-			if (GameUtils.getTeamSize().startsWith("No") || GameUtils.getTeamSize().startsWith("Open")) {
-				sender.sendMessage(Main.prefix() + "There are no matches running.");
-				return true;
-			}
-			
-			sender.sendMessage(Main.prefix() + "Match post: §a" + settings.getConfig().getString("matchpost"));
+		Settings settings = Settings.getInstance();
+		String teamSize = GameUtils.getTeamSize();
+		
+		if (teamSize.startsWith("No") || teamSize.startsWith("Open")) {
+			sender.sendMessage(Main.PREFIX + "There are no matches running.");
+			return true;
 		}
+		
+		sender.sendMessage(Main.PREFIX + "Match post: §a" + settings.getConfig().getString("matchpost"));
 		return true;
 	}
 }
