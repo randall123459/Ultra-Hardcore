beaa84fafc3db283b10f253f41703415b50e31a9
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:25:26 +0200

    Reworked /kick to work and look a lot better

--- src/com/leontg77/uhc/cmds/KickCommand.java
+++ src/com/leontg77/uhc/cmds/KickCommand.java
@@ -19,45 +19,43 @@ public class KickCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("kick")) {
-			if (sender.hasPermission("uhc.kick")) {
-				if (args.length < 2) {
-					sender.sendMessage(Main.prefix() + "Usage: /kick <player> <reason>");
-					return true;
-				}
-							
-				StringBuilder reason = new StringBuilder("");
-					
-				for (int i = 1; i < args.length; i++) {
-					reason.append(args[i]).append(" ");
-				}
-						
-				String msg = reason.toString().trim();
-
-				if (args[0].equals("*")) {
-					for (Player online : PlayerUtils.getPlayers()) {
-						if (!online.hasPermission("uhc.prelist")) {
-					    	online.kickPlayer("§8» §7" + msg + " §8«");
-						}
-					}
-					
-			    	PlayerUtils.broadcast(Main.prefix() + "§7All players has been kicked for §6" + msg, "uhc.admin");
-					return true;
-				}
-		    	
-		    	Player target = Bukkit.getServer().getPlayer(args[0]);
-				
-		    	if (target == null) {
-		    		sender.sendMessage(ChatColor.RED + "That player is not online.");
-		            return true;
-				}
-		    	
-		    	PlayerUtils.broadcast(Main.prefix() + "§6" + args[0] + " §7has been kicked for §a" + msg, "uhc.admin");
-		    	target.kickPlayer("§8» §7" + msg + " §8«");
-			} else {
-				sender.sendMessage(Main.NO_PERM_MSG);
-			}
+		if (!sender.hasPermission("uhc.kick")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
 		}
+		
+		if (args.length < 2) {
+			sender.sendMessage(Main.PREFIX + "Usage: /kick <player> <reason>");
+			return true;
+		}
+					
+		StringBuilder reason = new StringBuilder("");
+			
+		for (int i = 1; i < args.length; i++) {
+			reason.append(args[i]).append(" ");
+		}
+				
+		String msg = reason.toString().trim();
+
+		if (args[0].equals("*")) {
+			for (Player online : PlayerUtils.getPlayers()) {
+				if (!online.hasPermission("uhc.prelist")) {
+			    	online.kickPlayer("§8» §7" + msg + " §8«");
+				}
+			}
+			
+	    	PlayerUtils.broadcast(Main.PREFIX + "§7All players has been kicked for §6" + msg);
+			return true;
+		}
+    	
+    	Player target = Bukkit.getServer().getPlayer(args[0]);
+		
+    	if (target == null) {
+    		sender.sendMessage(ChatColor.RED + "That player is not online.");
+            return true;
+		}
+    	
+    	PlayerUtils.broadcast(Main.PREFIX + "§6" + args[0] + " §7has been kicked for §a" + msg);
+    	target.kickPlayer("§8» §7" + msg + " §8«");
 		return true;
 	}
 }
