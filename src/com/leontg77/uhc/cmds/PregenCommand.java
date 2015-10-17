3baeee5870dd8ad526680e9970388be80707b744
LeonTG77 <leontg77@gmail.com>
2015-10-18 01:58:52 +0200

    Did some work on /pregen

--- src/com/leontg77/uhc/cmds/PregenCommand.java
+++ src/com/leontg77/uhc/cmds/PregenCommand.java
@@ -3,6 +3,7 @@ package com.leontg77.uhc.cmds;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Difficulty;
 import org.bukkit.World;
@@ -15,26 +16,53 @@ import org.bukkit.command.CommandSender;
 
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Settings;
+import com.leontg77.uhc.worlds.Pregen;
 
 public class PregenCommand implements CommandExecutor {
-	private Settings config = Settings.getInstance();
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("setup")) {
-			if (sender.hasPermission("uhc.setup")) {
-				
-			} else {
-				sender.sendMessage(ChatColor.RED + "You do not have permission for this command.");
-			}
+		if (!sender.hasPermission("uhc.pregen")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
 		}
+		
+		Pregen pregen = Pregen.getInstance();
+		
+		if (args.length < 2) {
+			if (args.length > 0 && args[0].equalsIgnoreCase("cancel")) {
+				pregen.stop();
+				return true;
+			}
+			
+			sender.sendMessage(Main.PREFIX + "Usage: /pregen <world> <radius>");
+			return true;
+		}
+		
+		World world = Bukkit.getServer().getWorld(args[0]);
+		
+		if (world == null) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not an world.");
+			return true;
+		}
+		
+		int radius;
+		
+		try {
+			radius = Integer.parseInt(args[1]);
+		} catch (Exception e) {
+			sender.sendMessage(ChatColor.RED + args[1] + " is not a vaild radius.");
+			return true;
+		}
+		
+		pregen.start(world, radius);
 		return true;
 	}
 	
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("setup")) {
-			if (sender.hasPermission("uhc.setup")) {
+			if (sender.hasPermission("uhc.setup")) { 
 				if (args.length == 1) {
-		        	ArrayListtrshn<String> arg = new ArrayList<String>();
+		        	ArrayList<String> arg = new ArrayList<String>();
 		        	ArrayList<String> types = new ArrayList<String>();
 		        	types.add("cancel");
 		        	
