8781a08bebfd875be7e39fecddebcc2fd57a7b78
LeonTG77 <leontg77@gmail.com>
2015-10-17 12:12:52 +0200

    Reworked /hof to work and look better

--- src/com/leontg77/uhc/cmds/HOFCommand.java
+++ src/com/leontg77/uhc/cmds/HOFCommand.java
@@ -10,12 +10,18 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-import com.leontg77.uhc.InvGUI;
 import com.leontg77.uhc.Settings;
+import com.leontg77.uhc.inventory.InvGUI;
 import com.leontg77.uhc.utils.GameUtils;
 
+/**
+ * Hall of fame command class.
+ * 
+ * @author LeonTG77
+ */
 public class HOFCommand implements CommandExecutor, TabCompleter {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd,	String label, String[] args) {
 		if (!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.RED + "Only players can view the hall of fame.");
@@ -25,41 +31,41 @@ public class HOFCommand implements CommandExecutor, TabCompleter {
 		Player player = (Player) sender;
 		String host = GameUtils.getCurrentHost();
 		
-		if (cmd.getName().equalsIgnoreCase("hof")) {
-			if (args.length > 0) {
-				host = GameUtils.getHost(args[0]);
-			}
-			
-			if (Settings.getInstance().getHOF().getConfigurationSection(host) == null) {
-				sender.sendMessage(ChatColor.RED + "That player has not hosted any games here.");
-				return true;
-			}
-			
-			InvGUI.getManager().openHOF(player, host);
+		if (args.length > 0) {
+			host = GameUtils.getHost(args[0]);
 		}
+		
+		if (Settings.getInstance().getHOF().getConfigurationSection(host) == null) {
+			sender.sendMessage(ChatColor.RED + "That player has not hosted any games here.");
+			return true;
+		}
+		
+		InvGUI.getInstance().openHOF(player, host);
 		return true;
 	}
 	
+	@Override
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("hof")) {
-			if (args.length == 1) {
-	        	ArrayList<String> arg = new ArrayList<String>();
-	        	
-	        	if (!args[0].equals("")) {
-	        		for (String type : Settings.getInstance().getHOF().getKeys(false)) {
-	        			if (type.toLowerCase().startsWith(args[0].toLowerCase())) {
-	        				arg.add(type);
-	        			}
-	        		}
-	        	}
-	        	else {
-	        		for (String type : Settings.getInstance().getHOF().getKeys(false)) {
-        				arg.add(type);
-	        		}
-	        	}
-	        	return arg;
-	        }
+		if (!(sender instanceof Player)) {
+			return null;
 		}
-		return null;
+		
+    	ArrayList<String> toReturn = new ArrayList<String>();
+    	
+		if (args.length == 1) {
+        	if (args[0].equals("")) {
+        		for (String type : Settings.getInstance().getHOF().getKeys(false)) {
+    				toReturn.add(type);
+        		}
+        	} else {
+        		for (String type : Settings.getInstance().getHOF().getKeys(false)) {
+        			if (type.toLowerCase().startsWith(args[0].toLowerCase())) {
+        				toReturn.add(type);
+        			}
+        		}
+        	}
+        }
+		
+    	return toReturn;
 	}
 }
