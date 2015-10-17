5c31e5efd1aa1271fb3fd607e4189ef322fdfead
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:27:21 +0200

    Reworked /setmaxhealth to work and look a lot better

--- src/com/leontg77/uhc/cmds/SetmaxhealthCommand.java
+++ src/com/leontg77/uhc/cmds/SetmaxhealthCommand.java
@@ -8,64 +8,77 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.leontg77.uhc.Main;
+import com.leontg77.uhc.utils.NumberUtils;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * SetMaxhealth command class.
+ * 
+ * @author LeonTG77
+ */
 public class SetmaxhealthCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("setmaxhealth")) {
-			if (sender.hasPermission("uhc.setmaxhealth")) {
-				if (args.length == 0) {
-					sender.sendMessage(ChatColor.RED + "Usage: /setmaxhealth <health> [player|*]");
-					return true;
-				}
-				
-				double health;
-				
-				try {
-					health = Double.parseDouble(args[0]);
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "Invaild health.");
-					return true;
-				}
-				
-				if (health > 200) {
-					sender.sendMessage(ChatColor.RED + "You cannot set more than 200 health.");
-					return true;
-				}
-				
-				if (args.length == 1) {
-					if (sender instanceof Player) {
-						Player player = (Player) sender;
-						player.setMaxHealth(health);
-						player.sendMessage(Main.prefix() + "You set your own maxhealth to §6" + health);
-					} else {
-						sender.sendMessage(ChatColor.RED + "Only players can change their maxhealth.");
-					}
-					return true;
-				}
-				
-				if (args[1].equals("*")) {
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.setMaxHealth(health);
-					}
-					PlayerUtils.broadcast(Main.prefix() + "All players max health is now §6" + health);
-				} else {
-					Player target = Bukkit.getServer().getPlayer(args[1]);
-					
-					if (target == null) {
-						sender.sendMessage(ChatColor.RED + "That player is not online.");
-						return true;
-					}
-					
-					target.setMaxHealth(health);
-					target.sendMessage(Main.prefix() + "Your maxhealth was set to §6" + health);
-					sender.sendMessage(Main.prefix() + "You set §6" + target.getName() + "'s §7maxhealth to §6" + health);
-				}
-			} else {
-				sender.sendMessage(Main.NO_PERM_MSG);
-			}
+		if (!sender.hasPermission("uhc.setmaxhealth")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
 		}
+		
+		if (args.length == 0) {
+			sender.sendMessage(Main.PREFIX + "Usage: /setmaxhealth <health> [player|*]");
+			return true;
+		}
+		
+		double health;
+		
+		try {
+			health = Double.parseDouble(args[0]);
+		} catch (Exception e) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not a vaild health.");
+			return true;
+		}
+		
+		if (health < 1) {
+			health = 1;
+		}
+		
+		if (health > 2000) {
+			health = 2000;
+		}
+		
+		if (args.length == 1) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Only players can change their max health.");
+				return true;
+			}
+			
+			Player player = (Player) sender;
+			
+			player.setMaxHealth(health);
+			player.sendMessage(Main.PREFIX + "You set your max health to §6" + NumberUtils.makePercent(health));
+			return true;
+		}
+		
+		if (args[1].equals("*")) {
+			for (Player online : PlayerUtils.getPlayers()) {
+				online.setMaxHealth(health);
+			}
+			
+			PlayerUtils.broadcast(Main.PREFIX + "All players max health was set to §6" + NumberUtils.makePercent(health));
+			return true;
+		}
+		
+		Player target = Bukkit.getServer().getPlayer(args[1]);
+		
+		if (target == null) {
+			sender.sendMessage(ChatColor.RED + "That player is not online.");
+			return true;
+		}
+
+		sender.sendMessage(Main.PREFIX + "You set §6" + target.getName() + "'s §7max health to §6" + NumberUtils.makePercent(health));
+		target.sendMessage(Main.PREFIX + "Your max health was set to §6" + NumberUtils.makePercent(health));
+		target.setMaxHealth(health);
 		return true;
 	}
 }
