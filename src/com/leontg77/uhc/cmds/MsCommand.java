5ac3d86f774e2f616803eae3b37f57c2d806f4c8
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:25:44 +0200

    Reworked /ms to work and look a lot better

--- src/com/leontg77/uhc/cmds/MsCommand.java
+++ src/com/leontg77/uhc/cmds/MsCommand.java
@@ -10,29 +10,34 @@ import org.bukkit.entity.Player;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * Ms command class.
+ * 
+ * @author LeonTG77
+ */
 public class MsCommand implements CommandExecutor {
 	
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equals("ms")) {
-			if (args.length == 0) {
-				if (sender instanceof Player) {
-					Player player = (Player) sender;
-					player.sendMessage(Main.prefix() + "Your ping: §6" + PlayerUtils.getPing(player));
-				} else {
-					sender.sendMessage(ChatColor.RED + "Only players can check their ping.");
-				}
+		if (args.length == 0) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Only players can check their ping.");
 				return true;
 			}
 			
-			Player target = Bukkit.getServer().getPlayer(args[0]);
-			
-			if (target == null) {
-				sender.sendMessage(ChatColor.RED + "That player is not online.");
-				return true;
-			}
-			
-			sender.sendMessage(Main.prefix() + ChatColor.GOLD + target.getName() + "'s §7ping: §6" + PlayerUtils.getPing(target));
+			Player player = (Player) sender;
+			player.sendMessage(Main.PREFIX + "Your ping: §6" + PlayerUtils.getPing(player));
+			return true;
 		}
+		
+		Player target = Bukkit.getServer().getPlayer(args[0]);
+		
+		if (target == null) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not online.");
+			return true;
+		}
+		
+		sender.sendMessage(Main.PREFIX + target.getName() + "'s ping: §6" + PlayerUtils.getPing(target));
 		return true;
 	}
 }
