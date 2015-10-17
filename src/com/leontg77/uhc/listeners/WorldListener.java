29037a5a8a9cd17d431ccc698f95458aae20abc9
LeonTG77 <leontg77@gmail.com>
2015-10-17 12:19:38 +0200

    Some changed to make it more efficient

--- src/com/leontg77/uhc/listeners/WorldListener.java
+++ src/com/leontg77/uhc/listeners/WorldListener.java
@@ -1,25 +1,27 @@
 package com.leontg77.uhc.listeners;
 
+import java.util.List;
+
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.ThunderChangeEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.event.world.ChunkPopulateEvent;
 import org.bukkit.event.world.WorldInitEvent;
 
-import com.leontg77.uhc.AntiStripmine;
 import com.leontg77.uhc.Main;
-import com.leontg77.uhc.Timers;
 import com.leontg77.uhc.State;
-import com.leontg77.uhc.AntiStripmine.ChunkOreRemover;
-import com.leontg77.uhc.AntiStripmine.WorldData;
+import com.leontg77.uhc.Timers;
+import com.leontg77.uhc.utils.GameUtils;
+import com.leontg77.uhc.worlds.terrain.AntiStripmine;
+import com.leontg77.uhc.worlds.terrain.AntiStripmine.ChunkOreRemover;
+import com.leontg77.uhc.worlds.terrain.AntiStripmine.WorldData;
 
 /**
- * Weather listener class.
+ * World listener class.
  * <p>
- * Contains all eventhandlers for weather releated events.
+ * Contains all eventhandlers for world releated events.
  * 
  * @author LeonTG77
  */
@@ -27,36 +29,43 @@ public class WorldListener implements Listener {
 
 	@EventHandler
 	public void onWeatherChange(WeatherChangeEvent event) {
-    	if (event.toWeatherState()) {
-			if (event.getWorld().getName().equals("lobby") || event.getWorld().getName().equalsIgnoreCase("arena")) {
-				event.setCancelled(true);
-				return;
-			}
+    	if (!event.toWeatherState()) {
+    		return;
+    	}
+    	
+    	List<World> worlds = GameUtils.getGameWorlds();
+    	World world = event.getWorld();
+    	
+    	if (!worlds.contains(world)) {
+			event.setCancelled(true);
+			return;
+		}
 
-			if (!State.isState(State.INGAME)) {
-				event.setCancelled(true);
-				return;
-			}
+		if (!State.isState(State.INGAME)) {
+			event.setCancelled(true);
+			return;
+		}
 
-			if (Timers.meetup <= 0) {
-				event.setCancelled(true);
-				return;
-			}
+		if (Timers.meetup <= 0) {
+			event.setCancelled(true);
+			return;
+		}
 
-			if (Timers.pvp > 0) {
-				event.setCancelled(true);
-			}
+		if (Timers.pvp > 0) {
+			event.setCancelled(true);
 		}
 	}
 
 	@EventHandler
 	public void onThunderChange(ThunderChangeEvent event) {
-    	if (event.toThunderState()) {
-			event.setCancelled(true);
+    	if (!event.toThunderState()) {
+    		return;
 		}
+    	
+		event.setCancelled(true);
 	}
 
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	@EventHandler
 	public void onChunkPopulate(ChunkPopulateEvent event) {
 		AntiStripmine strip = AntiStripmine.getInstance();
 		World world = event.getWorld();
@@ -78,7 +87,7 @@ public class WorldListener implements Listener {
 		}
 	}
 
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	@EventHandler
 	public void onWorldInit(WorldInitEvent event) {
 		AntiStripmine strip = AntiStripmine.getInstance();
 		World world = event.getWorld();
