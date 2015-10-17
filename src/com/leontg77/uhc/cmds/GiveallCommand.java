8b4b4c8afd4d75dbf76464cf565aa96270782d1f
LeonTG77 <leontg77@gmail.com>
2015-10-17 12:08:07 +0200

    Reworked the /giveall command

--- src/com/leontg77/uhc/cmds/GiveallCommand.java
+++ src/com/leontg77/uhc/cmds/GiveallCommand.java
@@ -12,65 +12,70 @@ import org.bukkit.inventory.ItemStack;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * GiveAll command class.
+ * 
+ * @author LeonTG77
+ */
 @SuppressWarnings("deprecation")
 public class GiveallCommand implements CommandExecutor {	
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("giveall")) {
-			if (sender.hasPermission("uhc.giveall")) {
-				if (args.length == 0) {
-					sender.sendMessage(Main.prefix() + "Usage: /giveall <item> [amount] [durability]");
-					return true;
+		if (!sender.hasPermission("uhc.giveall")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
+		}
+		
+		if (args.length == 0) {
+			sender.sendMessage(Main.PREFIX + "Usage: /giveall <item> [amount] [durability]");
+			return true;
+		}
+		
+		Material material = null;
+		int amount = 1;
+		short durability = 0;
+		
+		try {
+			material = Material.getMaterial(Integer.parseInt(args[0]));
+		} 
+		catch (Exception e) {
+			for (Material types : Material.values()) {
+				if (types.name().startsWith(args[0].toUpperCase())) {
+					material = types;
+					break;
 				}
-				
-				Material material = null;
-				int amount = 1;
-				short durability = 0;
-				
-				try {
-					material = Material.getMaterial(Integer.parseInt(args[0]));
-				} 
-				catch (Exception e) {
-					for (Material types : Material.values()) {
-						if (types.name().startsWith(args[0].toUpperCase())) {
-							material = types;
-							break;
-						}
-					}
-				}
-				
-				if (material == null) {
-					sender.sendMessage(ChatColor.RED + "Unknown item name.");
-					return true;
-				}
-				
-				if (args.length > 1) {
-					try {
-						amount = Integer.parseInt(args[1]);
-					} catch (Exception e) {
-						sender.sendMessage(ChatColor.RED + "That is not a vaild amount.");
-						return true;
-					}
-				}
-				
-				if (args.length > 2) {
-					try {
-						durability = Short.parseShort(args[2]);
-					} catch (Exception e) {
-						sender.sendMessage(ChatColor.RED + "That is not a vaild durability.");
-						return true;
-					}
-				}
-
-				ItemStack item = new ItemStack(material, amount, durability);
-				PlayerUtils.broadcast(Main.prefix() + "You've been given §a" + amount + " §7of §6" + item.getType().name().toLowerCase().replaceAll("_", " ") + "§7.");
-				
-				for (Player online : Bukkit.getServer().getOnlinePlayers()) {
-					PlayerUtils.giveItem(online, item);
-				}
-			} else {
-				sender.sendMessage(Main.NO_PERM_MSG);
 			}
+		}
+		
+		if (material == null) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not a vaild type.");
+			return true;
+		}
+		
+		if (args.length > 1) {
+			try {
+				amount = Integer.parseInt(args[1]);
+			} catch (Exception e) {
+				sender.sendMessage(ChatColor.RED + args[1] + " is not a vaild number.");
+				return true;
+			}
+		}
+		
+		if (args.length > 2) {
+			try {
+				durability = Short.parseShort(args[2]);
+			} catch (Exception e) {
+				sender.sendMessage(ChatColor.RED + args[2] + " is not a vaild number.");
+				return true;
+			}
+		}
+
+		ItemStack item = new ItemStack(material, amount, durability);
+		PlayerUtils.broadcast(Main.PREFIX + "All players recieved §a" + amount + " " + item.getType().name().toLowerCase().replaceAll("_", " ") + (amount > 1 ? "s" : "") + "§7.");
+		
+		for (Player online : Bukkit.getServer().getOnlinePlayers()) {
+			PlayerUtils.giveItem(online, item);
 		}
 		return true;
 	}
