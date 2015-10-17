92bdccc64f45b6d02c20c06752196ca15778793c
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:31:40 +0200

    Reworked /tl to work and look a lot better

--- src/com/leontg77/uhc/cmds/TlCommand.java
+++ src/com/leontg77/uhc/cmds/TlCommand.java
@@ -1,6 +1,5 @@
 package com.leontg77.uhc.cmds;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,9 +8,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
 
 import com.leontg77.uhc.Spectator;
+import com.leontg77.uhc.Teams;
 
+/**
+ * TeamLoc command class.
+ * 
+ * @author LeonTG77
+ */
 public class TlCommand implements CommandExecutor {
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.RED + "Only players can display their location to their.");
@@ -20,27 +26,17 @@ public class TlCommand implements CommandExecutor {
 		
 		Player player = (Player) sender;
 		
-		if (cmd.getName().equalsIgnoreCase("teamloc")) {
-			if (Spectator.getInstance().isSpectating(player)) {
-				player.sendMessage(ChatColor.RED + "You can't do this as a spectator.");
-	        	return true;
-	        }
-			
-			Team team = player.getScoreboard().getEntryTeam(player.getName());
-			
-			if (team == null) { 
-	        	player.sendMessage(ChatColor.RED + "You are not on a team.");
-	        	return true;
-			} 
-			
-			for (String entry : player.getScoreboard().getEntryTeam(player.getName()).getEntries()) {
-				Player teammate = Bukkit.getServer().getPlayer(entry);
-				
-				if (teammate != null) {
-					teammate.sendMessage("§9§lTeam §8» §7" + player.getName() + "'s coords: §fx:" + player.getLocation().getBlockX() + " y:" + player.getLocation().getBlockY() + " z:" + player.getLocation().getBlockZ() + " (" + player.getWorld().getEnvironment().name().replaceAll("_", " ").replaceAll("NORMAL", "overworld").toLowerCase().replaceAll("normal", "overworld") + ")");
-				}
-			}
-		}
+		Spectator spec = Spectator.getInstance();
+		Teams teams = Teams.getInstance();
+		
+		Team team = teams.getTeam(player);
+		
+		if (team == null || spec.isSpectating(player)) { 
+        	player.sendMessage(ChatColor.RED + "You are not on a team.");
+        	return true;
+		} 
+		
+		teams.sendMessage(team, "§9§lTeam §8» §7" + player.getName() + "'s loc: §fx:" + player.getLocation().getBlockX() + " y:" + player.getLocation().getBlockY() + " z:" + player.getLocation().getBlockZ() + " (" + player.getWorld().getEnvironment().name().replaceAll("_", " ").replaceAll("NORMAL", "overworld").toLowerCase().replaceAll("normal", "overworld") + ")");
 		return true;
 	}
 }
