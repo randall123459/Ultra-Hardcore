846d49a3a3a2aa2ff21822e0aaa2d8638e5e9a37
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:27:28 +0200

    Reworked /list to work and look a lot better

--- src/com/leontg77/uhc/cmds/ListCommand.java
+++ src/com/leontg77/uhc/cmds/ListCommand.java
@@ -3,6 +3,7 @@ package com.leontg77.uhc.cmds;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,42 +13,47 @@ import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Settings;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * List command class.
+ * 
+ * @author LeonTG77
+ */
 public class ListCommand implements CommandExecutor {
-	private Settings settings = Settings.getInstance();
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, final String[] args) {
-		if (cmd.getName().equalsIgnoreCase("list")) {
-			if (PlayerUtils.getPlayers().size() < 1) {
-		    	sender.sendMessage(Main.prefix() + "There are no players online.");
-				return true;
-			}
-			
-			ArrayList<Player> players = new ArrayList<Player>(PlayerUtils.getPlayers());
-			Collections.shuffle(players);
-			
-	    	StringBuilder list = new StringBuilder();
-	    	int p = 1;
-	    		
-	    	for (int i = 0; i < players.size(); i++) {
-	    		if (sender instanceof Player && !((Player) sender).canSee(players.get(i))) {
-	    			continue;
-	    		}
-	    		
-				if (list.length() > 0) {
-					if (p == players.size()) {
-						list.append(" §8and §a");
-					} else {
-						list.append("§8, §a");
-					}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (PlayerUtils.getPlayers().size() < 1) {
+	    	sender.sendMessage(Main.PREFIX + "There are no players online.");
+			return true;
+		}
+
+		Settings settings = Settings.getInstance();
+		
+		ArrayList<Player> players = new ArrayList<Player>(PlayerUtils.getPlayers());
+		Collections.shuffle(players);
+		
+    	StringBuilder list = new StringBuilder();
+    	int p = 1;
+    		
+    	for (int i = 0; i < players.size(); i++) {
+    		if (sender instanceof Player && !((Player) sender).canSee(players.get(i))) {
+    			continue;
+    		}
+    		
+			if (list.length() > 0) {
+				if (p == players.size()) {
+					list.append(" §8and §a");
+				} else {
+					list.append("§8, §a");
 				}
-				
-				list.append(players.get(i).getName());
-				p++;
 			}
-	    			
-	    	sender.sendMessage(Main.prefix() + "There are §6" + (p - 1) + " §7out of§6 " + settings.getConfig().getInt("maxplayers") + " §7players online.");
-	    	sender.sendMessage("§8» §7Players§8: §a" + list.toString() + "§8.");
-        }
+			
+			list.append(players.get(i).getName());
+			p++;
+		}
+    			
+    	sender.sendMessage(Main.PREFIX + "There are §6" + (p - 1) + " §7out of§6 " + settings.getConfig().getInt("maxplayers", Bukkit.getMaxPlayers()) + " §7players online.");
+    	sender.sendMessage("§8» §7Players§8: §a" + list.toString() + "§8.");
 		return true;
 	}
 }
