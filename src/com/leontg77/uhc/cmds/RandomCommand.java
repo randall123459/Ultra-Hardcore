c8f5398ebf9c5f9c9bb3cac7fb61a702de3c0ab3
LeonTG77 <leontg77@gmail.com>
2015-10-17 18:26:54 +0200

    Reworked /random to work and look a lot better

--- src/com/leontg77/uhc/cmds/RandomCommand.java
+++ src/com/leontg77/uhc/cmds/RandomCommand.java
@@ -15,121 +15,87 @@ import com.leontg77.uhc.Main;
 import com.leontg77.uhc.Teams;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * Random command class.
+ * 
+ * @author LeonTG77
+ */
 public class RandomCommand implements CommandExecutor {
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, final String[] args) {
-		if (cmd.getName().equalsIgnoreCase("random")) {
-			if (sender.hasPermission("uhc.random")) {
-				if (args.length == 0) {
-					sender.sendMessage(Main.prefix() + "Usage: /random <size> [playersnotplaying...]");
-					return true;
-				}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!sender.hasPermission("uhc.random")) {
+			sender.sendMessage(Main.NO_PERM_MSG);
+			return true;
+		}
+		
+		if (args.length == 0) {
+			sender.sendMessage(Main.PREFIX + "Usage: /random <size> [playersnotplaying...]");
+			return true;
+		}
+		
+		int size = 1;
+		
+		try {
+			size = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			sender.sendMessage(ChatColor.RED + args[0] + " is not a vaild teamsize.");
+			return true;
+		}
+		
+		ArrayList<Player> list = new ArrayList<Player>();
+		
+		for (Player online : PlayerUtils.getPlayers()) {
+			if (online.getScoreboard().getEntryTeam(online.getName()) == null) {
+				list.add(online);
+			}
+		}
+		
+		Collections.shuffle(list);
+		
+		if (args.length > 1) {
+			for (int i = 1; i < args.length; i++) {
+				Player target = Bukkit.getServer().getPlayer(args[i]);
 				
-				int size = 1;
-				
-				try {
-					size = Integer.parseInt(args[0]);
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "Invaild team.");
-					return true;
-				}
-				
-				if (args.length == 1) {
-					ArrayList<Player> a = new ArrayList<Player>();
-					
-					for (Player online : PlayerUtils.getPlayers()) {
-						if (online.getScoreboard().getEntryTeam(online.getName()) == null) {
-							a.add(online);
-						}
+				if (target != null) {
+					if (list.contains(target)) {
+						list.remove(target);
 					}
-					
-					Collections.shuffle(a);
+				}
+			}
+		}
 
-					Team t = Teams.getInstance().findAvailableTeam();
-					
-					if (t == null) {
-						sender.sendMessage(ChatColor.RED + "No more available teams.");
-						return true;
-					}
-					
-					try {
-						for (int i = 0; i < size; i++) {
-							if (a.size() < i) {
-								sender.sendMessage(ChatColor.RED + "Could not add a player to team " + t.getName() + ".");
-								continue;
-							}
-							
-							Player p = a.get(i);
-							t.addEntry(p.getName());
-						}
-					} catch (Exception e) {
-						sender.sendMessage(ChatColor.RED + "Not enough players for this team.");
-					}
+		Teams teams = Teams.getInstance();
+		Team team = teams.findAvailableTeam();
+		
+		if (team == null) {
+			sender.sendMessage(ChatColor.RED + "No more available teams.");
+			return true;
+		}
+		
+		try {
+			for (int i = 0; i < size; i++) {
+				if (list.size() < i) {
+					sender.sendMessage(ChatColor.RED + "Could not add a player to team " + team.getName() + ".");
+					continue;
+				}
+				
+				Player p = list.get(i);
+				team.addEntry(p.getName());
+				p.sendMessage(Main.PREFIX + "You were added to team " + team.getName());
+			}
+		} catch (Exception e) {
+			sender.sendMessage(ChatColor.RED + "Not enough players for this team.");
+		}
 
-					if (t.getSize() > 0) {
-						sender.sendMessage(Main.prefix() + "Created a rTo" + size + " using team " + t.getName() + ".");
-						Teams.getInstance().sendMessage(t, Main.prefix() + "You were added to §a" + t.getName() + "§7.");
-						Teams.getInstance().sendMessage(t, Main.prefix() + "Your teammates:");
-						
-						for (String entry : t.getEntries()) {
-							Teams.getInstance().sendMessage(t, Main.prefix() + "§a" + entry);
-						}
-					}
-					return true;
-				}
-				
-				ArrayList<Player> a = new ArrayList<Player>();
-				
-				for (Player online : PlayerUtils.getPlayers()) {
-					if (online.getScoreboard().getEntryTeam(online.getName()) == null) {
-						a.add(online);
-					}
-				}
-				
-				Collections.shuffle(a);
-				
-				for (int i = 1; i < args.length; i++) {
-					Player target = Bukkit.getServer().getPlayer(args[i]);
-					
-					if (target != null) {
-						if (a.contains(target)) {
-							a.remove(target);
-						}
-					}
-				}
-
-				Team t = Teams.getInstance().findAvailableTeam();
-				
-				if (t == null) {
-					sender.sendMessage(ChatColor.RED + "No more available teams.");
-					return true;
-				}
-				
-				try {
-					for (int i = 0; i < size; i++) {
-						if (a.size() < i) {
-							sender.sendMessage(ChatColor.RED + "Could not add a player to team " + t.getName() + ".");
-							continue;
-						}
-						
-						Player p = a.get(i);
-						t.addEntry(p.getName());
-						p.sendMessage(Main.prefix() + "You were added to team " + t.getName());
-					}
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "Not enough players for this team.");
-				}
-
-				if (t.getSize() > 0) {
-					sender.sendMessage(Main.prefix() + "Created a rTo" + size + " using team " + t.getName() + ".");
-					Teams.getInstance().sendMessage(t, Main.prefix() + "You were added to §a" + t.getName() + "§7.");
-					Teams.getInstance().sendMessage(t, Main.prefix() + "Your teammates:");
-					for (String entry : t.getEntries()) {
-						Teams.getInstance().sendMessage(t, Main.prefix() + "§a" + entry);
-					}
-				}
-			} else {
-				sender.sendMessage(Main.NO_PERM_MSG);
+		if (team.getSize() > 0) {
+			sender.sendMessage(Main.PREFIX + "Created a team of " + size + " using team " + team.getName() + ".");
+			
+			teams.sendMessage(team, Main.PREFIX + "You were added to §6" + team.getName() + "§7.");
+			teams.sendMessage(team, Main.PREFIX + "Your teammates:");
+			
+			for (String entry : team.getEntries()) {
+				teams.sendMessage(team, Main.PREFIX + "§a" + entry);
 			}
 		}
 		return true;
