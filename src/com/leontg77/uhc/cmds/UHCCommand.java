3deb95c53fcf7737d05af6bbd3d47860310dfe31
LeonTG77 <leontg77@gmail.com>
2015-10-21 08:12:25 +0200

    Moved InvGUI back to main package

--- src/com/leontg77/uhc/cmds/UHCCommand.java
+++ src/com/leontg77/uhc/cmds/UHCCommand.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.leontg77.uhc.inventory.InvGUI;
+import com.leontg77.uhc.InvGUI;
 
 /**
  * UHC command class.
