d61f0c787ceb9efadf662572917cfa44e7fde27c
LeonTG77 <leontg77@gmail.com>
2015-10-21 08:16:06 +0200

    Changed way of getting the matchpost

--- src/com/leontg77/uhc/cmds/MatchpostCommand.java
+++ src/com/leontg77/uhc/cmds/MatchpostCommand.java
@@ -4,8 +4,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import com.leontg77.uhc.Game;
 import com.leontg77.uhc.Main;
-import com.leontg77.uhc.Settings;
 import com.leontg77.uhc.utils.GameUtils;
 
 /**
@@ -17,15 +17,15 @@ public class MatchpostCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		Settings settings = Settings.getInstance();
 		String teamSize = GameUtils.getTeamSize();
+		Game game = Game.getInstance();
 		
 		if (teamSize.startsWith("No") || teamSize.startsWith("Open")) {
 			sender.sendMessage(Main.PREFIX + "There are no matches running.");
 			return true;
 		}
 		
-		sender.sendMessage(Main.PREFIX + "Match post: §a" + settings.getConfig().getString("matchpost"));
+		sender.sendMessage(Main.PREFIX + "Match post: §a" + game.getMatchPost());
 		return true;
 	}
 }
