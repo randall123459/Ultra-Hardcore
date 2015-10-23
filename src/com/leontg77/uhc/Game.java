0001a63b26bb89f4a2bc4d4eb75fc6696eb2feaf
LeonTG77 <leontg77@gmail.com>
2015-10-23 17:57:02 +0200

    Progress update on the rework of the game class

--- src/com/leontg77/uhc/Game.java
+++ src/com/leontg77/uhc/Game.java
@@ -8,8 +8,14 @@ import com.leontg77.uhc.Main.BorderShrink;
 import com.leontg77.uhc.utils.PacketUtils;
 import com.leontg77.uhc.utils.PlayerUtils;
 
+/**
+ * Game management class.
+ * <p>
+ * This class contains all setters and getters for all togglable features.
+ * 
+ * @author LeonTG77
+ */
 public class Game {
-	private Settings settings = Settings.getInstance();
 	private static Game instance = new Game();
 	
 	/**
@@ -20,6 +26,11 @@ public class Game {
 	public static Game getInstance() {
 		return instance;
 	}
+
+	private Settings settings = Settings.getInstance();
+	
+	// ############################ BASIC ############################
+	// TODO: <-- just for the marker.
 	
 	/**
 	 * Change the FFA mode.
@@ -27,8 +38,12 @@ public class Game {
 	 * @param ffa True to enable, false to disable.
 	 */
 	public void setFFA(boolean ffa) {
-		settings.getConfig().set("game.ffa", ffa);
+		settings.getConfig().set("ffa", ffa);
 		settings.saveConfig();
+		
+		for (Player online : PlayerUtils.getPlayers()) {
+			PacketUtils.setTabList(online);
+		}
 	}
 	
 	/**
@@ -37,7 +52,7 @@ public class Game {
 	 * @return True if it is, false otherwise.
 	 */
 	public boolean isFFA() {
-		return settings.getConfig().getBoolean("game.ffa", true);
+		return settings.getConfig().getBoolean("ffa", true);
 	}
 	
 	/**
@@ -46,8 +61,12 @@ public class Game {
 	 * @param teamSize the new teamsize.
 	 */
 	public void setTeamSize(int teamSize) {
-		settings.getConfig().set("game.teamsize", teamSize);
+		settings.getConfig().set("teamsize", teamSize);
 		settings.saveConfig();
+		
+		for (Player online : PlayerUtils.getPlayers()) {
+			PacketUtils.setTabList(online);
+		}
 	}
 	
 	/**
@@ -56,7 +75,348 @@ public class Game {
 	 * @return The teamsize.
 	 */
 	public int getTeamSize() {
-		return settings.getConfig().getInt("game.teamsize", 0);
+		return settings.getConfig().getInt("teamsize", 0);
+	}
+	
+	/**
+	 * Set the scenarios of the game.
+	 * 
+	 * @param scenarios The new scenarios.
+	 */
+	public void setScenarios(String scenarios) {
+		settings.getConfig().set("scenarios", scenarios);
+		settings.saveConfig();
+		
+		for (Player online : PlayerUtils.getPlayers()) {
+			PacketUtils.setTabList(online);
+		}
+	}
+
+	public String getScenarios() {
+		return settings.getConfig().getString("scenarios", "games scheduled");
+	}
+	
+	/**
+	 * Set the host of the game.
+	 * 
+	 * @param host The new host.
+	 */
+	public void setHost(String host) {
+		settings.getConfig().set("host", host);
+		settings.saveConfig();
+		
+		for (Player online : PlayerUtils.getPlayers()) {
+			PacketUtils.setTabList(online);
+		}
+	}
+
+	/**
+	 * Get the host of the game.
+	 * 
+	 * @return The host.
+	 */
+	public String getHost() {
+		return settings.getConfig().getString("host", "None");
+	}
+
+	/**
+	 * Set the max players that will be able to join.
+	 * 
+	 * @param maxplayers The max player limit.
+	 */
+	public void setMaxPlayers(int maxplayers) {
+		settings.getConfig().set("maxplayers", maxplayers);
+		settings.saveConfig();
+	}
+	
+	/**
+	 * Get the max players that can join the server.
+	 * 
+	 * @return The max player limit.
+	 */
+	public int getMaxPlayers() {
+		return settings.getConfig().getInt("maxplayers", 150);
+	}
+
+	/**
+	 * Set the matchpost for the game.
+	 * 
+	 * @param matchpost The new matchpost.
+	 */
+	public void setMatchPost(String matchpost) {
+		settings.getConfig().set("matchpost", matchpost);
+		settings.saveConfig();
+	}
+
+	/**
+	 * Get the matchpost for the game.
+	 * 
+	 * @return The game's matchpost.
+	 */
+	public String getMatchPost() {
+		return settings.getConfig().getString("matchpost", "No_Post_Set");
+	}
+	
+	/**
+	 * Set the world to be used for the game.
+	 * <p> 
+	 * This will automaticly use all the same worlds 
+	 * that has the world name with _end or _nether at the end.
+	 * 
+	 * @param name The new world name.
+	 */
+	public void setWorld(String name) {
+		settings.getConfig().set("world", name);
+		settings.saveConfig();
+	}
+
+	/**
+	 * Get the world to be used for the game.
+	 * 
+	 * @return The game world
+	 */
+	public World getWorld() {
+		return Bukkit.getWorld(settings.getConfig().getString("world", "leon"));
+	}
+
+	/**
+	 * Set the time of the pvp for the game.
+	 * 
+	 * @param meetup The time in minutes.
+	 */
+	public void setPvP(int pvp) {
+		settings.getConfig().set("time.pvp", pvp);
+		settings.saveConfig();
+	}
+
+	/**
+	 * Get the time when pvp will be enabled after the start.
+	 * 
+	 * @return The time in minutes.
+	 */
+	public int getPvP() {
+		return settings.getConfig().getInt("time.pvp", 15);
+	}
+
+	/**
+	 * Set the time of the meetup of the game.
+	 * 
+	 * @param meetup The time in minutes.
+	 */
+	public void setMeetup(int meetup) {
+		settings.getConfig().set("time.meetup", meetup);
+		settings.saveConfig();
+	}
+
+	/**
+	 * Get the time when meetup will occur after the start.
+	 * 
+	 * @return The time in minutes.
+	 */
+	public int getMeetup() {
+		return settings.getConfig().getInt("time.meetup", 90);
+	}
+
+	// ############################ MISC ############################
+	// TODO: <-- just for the marker.
+
+	/**
+	 * Enable or disable team management.
+	 * 
+	 * @param enable True to enable, false to disable.
+	 */
+	public void setTeamManagement(boolean enable) {
+		settings.getConfig().set("misc.team", enable);
+		settings.saveConfig();
+	}
+	
+	/**
+	 * Get if team management is enabled.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public boolean teamManagement() {
+		return settings.getConfig().getBoolean("misc.team", false);
+	}
+
+	/**
+	 * Enable or disable the pregame board.
+	 * 
+	 * @param enable True to enable, false to disable.
+	 */
+	public void setPregameBoard(boolean enable) {
+		settings.getConfig().set("misc.board.pregame", enable);
+		settings.saveConfig();
+	}
+
+	/**
+	 * Get if the pregame board is enabled.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public boolean pregameBoard() {
+		return settings.getConfig().getBoolean("misc.board.pregame", false);
+	}
+
+	/**
+	 * Enable or disable the arena board.
+	 * 
+	 * @param enable True to enable, false to disable.
+	 */
+	public void setArenaBoard(boolean enable) {
+		settings.getConfig().set("misc.board.arena", enable);
+		settings.saveConfig();
+	}
+
+	/**
+	 * Get if the arena board is enabled.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public boolean arenaBoard() {
+		return settings.getData().getBoolean("misc.board.arena", false);
+	}
+
+	/**
+	 * Enable or disable recordedround mode.
+	 * 
+	 * @param enable True to enable, false to disable.
+	 */
+	public void setRecordedRound(boolean enable) {
+		settings.getConfig().set("recordedround.enabled", enable);
+		settings.saveConfig();
+		
+		for (Player online : PlayerUtils.getPlayers()) {
+			PacketUtils.setTabList(online);
+		}
+	}
+
+	/**
+	 * Get if recordedround mode is enabled.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public boolean isRecordedRound() {
+		return settings.getConfig().getBoolean("recordedround.enabled", false);
+	}
+	
+	/**
+	 * Set the name of the recorded round.
+	 * <p>
+	 * Note: If recordedround mode is disabled, this will have no effect.
+	 * 
+	 * @param name The new name of the RR.
+	 */
+	public void setRRName(String name) {
+		settings.getConfig().set("recordedround.name", name);
+		settings.saveConfig();
+	}
+
+	/**
+	 * Get the recorded round name.
+	 * @return
+	 */
+	public String getRRName() {
+		return settings.getConfig().getString("recordedround.name", "ANAMEHERE");
+	}
+
+	/**
+	 * Enable or disable stats.
+	 * 
+	 * @param enable True to enable, false to disable.
+	 */
+	public void setStats(boolean enable) {
+		settings.getConfig().set("misc.stats", enable);
+		settings.saveConfig();
+	}
+
+	/**
+	 * Get if stats are enabled.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public boolean stats() {
+		return settings.getConfig().getBoolean("misc.stats", true);
+	}
+
+	/**
+	 * Mute or unmute the chat
+	 * 
+	 * @param mute True to mute, false to unmute.
+	 */
+	public void setMuted(boolean mute) {
+		settings.getData().set("muted", mute);
+		settings.saveData();
+	}
+
+	/**
+	 * Check if the chat is muted.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public boolean isMuted() {
+		return settings.getData().getBoolean("muted", false);
+	}
+
+	// ############################ RATES ############################
+	// TODO: <-- just for the marker.
+
+	public void setAppleRates(int shearRate) {
+		settings.getConfig().set("rates.apple.rate", shearRate);
+		settings.saveConfig();
+	}
+	
+	public int getAppleRates() {
+		return settings.getConfig().getInt("rates.apple.rate", 1);
+	}
+	
+	public void setFlintRates(int flintRate) {
+		settings.getConfig().set("rates.flint.rate", flintRate);
+		settings.saveConfig();
+	}
+	
+	public int getFlintRates() {
+		return settings.getConfig().getInt("rates.flint.rate", 35);
+	}
+
+	/**
+	 * Enable or disable team management.
+	 * 
+	 * @param enable True to enable, false to disable.
+	 */
+	public void setShears(boolean enable) {
+		settings.getConfig().set("rates.shears.enabled", enable);
+		settings.saveConfig();
+	}
+	
+	/**
+	 * Get if team management is enabled.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public boolean shears() {
+		return settings.getConfig().getBoolean("rates.shears.enabled", true);
+	}
+	
+	public void setShearRates(int shearRate) {
+		settings.getConfig().set("rates.shears.rate", shearRate);
+		settings.saveConfig();
+	}
+	
+	public int getShearRates() {
+		if (!shears()) {
+			return 0;
+		}
+		
+		return settings.getConfig().getInt("rates.shears.rate", 5);
+	}
+	
+	// ############################ FEATURES ############################
+	// TODO: <-- just for the marker.
+
+	public boolean antiStripmine() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 	public void setBorderShrink(BorderShrink border) {
@@ -113,24 +473,6 @@ public class Game {
 		settings.saveConfig();
 	}
 
-	public String getScenarios() {
-		return settings.getConfig().getString("game.scenarios", "games scheduled");
-	}
-	
-	public void setScenarios(String sceanrios) {
-		settings.getConfig().set("game.scenarios", sceanrios);
-		settings.saveConfig();
-	}
-
-	public boolean isMuted() {
-		return settings.getConfig().getBoolean("muted", false);
-	}
-
-	public void setMuted(boolean mute) {
-		settings.getConfig().set("muted", mute);
-		settings.saveConfig();
-	}
-
 	public boolean deathLightning() {
 		return settings.getConfig().getBoolean("feature.deathlightning.enabled", true);
 	}
@@ -177,101 +519,21 @@ public class Game {
 	}
 
 	public boolean tabShowsHealthColor() {
-		return settings.getConfig().getBoolean("feature.tabcolors.enabled", false);
+		return settings.getConfig().getBoolean("feature.tabShowsHealthColor.enabled", false);
 	}
 	
 	public void setTabShowsHealthColor(boolean enable) {
-		settings.getConfig().set("feature.tabcolors.enabled", enable);
+		settings.getConfig().set("feature.tabShowsHealthColor.enabled", enable);
 		settings.saveConfig();
 	}
 
 	public boolean goldenMelonNeedsIngots() {
-		return settings.getConfig().getBoolean("feature.harderCrafting.enabled", true);
+		return settings.getConfig().getBoolean("feature.goldenMelonNeedsIngots.enabled", true);
 	}
 	
 	public void setGoldenMelonNeedsIngots(boolean enable) {
-		settings.getConfig().set("feature.harderCrafting.enabled", enable);
+		settings.getConfig().set("feature.goldenMelonNeedsIngots.enabled", enable);
 		settings.saveConfig();
-	}
-
-	public boolean shears() {
-		return settings.getConfig().getBoolean("rates.shears.enabled", true);
-	}
-	
-	public void setShears(boolean enable) {
-		settings.getConfig().set("rates.shears.enabled", enable);
-		settings.saveConfig();
-	}
-	
-	public int getShearRates() {
-		return settings.getConfig().getInt("rates.shears.rate", 5);
-	}
-	
-	public void setShearRates(int shearRate) {
-		settings.getConfig().set("rates.shears.rate", shearRate);
-		settings.saveConfig();
-	}
-	
-	public int getFlintRates() {
-		return settings.getConfig().getInt("rates.flint.rate", 35);
-	}
-	
-	public void setFlintRates(int flintRate) {
-		settings.getConfig().set("rates.flint.rate", flintRate);
-		settings.saveConfig();
-	}
-
-	public boolean pregameBoard() {
-		return settings.getData().getBoolean("boards.pregame", false);
-	}
-	
-	public void setPregameBoard(boolean enable) {
-		settings.getData().set("boards.pregame", enable);
-		settings.saveConfig();
-	}
-
-	public boolean arenaBoard() {
-		return settings.getData().getBoolean("boards.arena", false);
-	}
-	
-	public void setArenaBoard(boolean enable) {
-		settings.getData().set("boards.arena", enable);
-		settings.saveData();
-	}
-
-	public boolean teamManagement() {
-		return settings.getData().getBoolean("teams.management", false);
-	}
-	
-	public void setTeamManagement(boolean enable) {
-		settings.getData().set("teams.management", enable);
-		settings.saveData();
-	}
-
-	public boolean isRecordedRound() {
-		return settings.getConfig().getBoolean("rr.state", false);
-	}
-	
-	public void setRR(boolean enable) {
-		settings.getConfig().set("rr.state", enable);
-		settings.saveData();
-		
-		for (Player online : PlayerUtils.getPlayers()) {
-			PacketUtils.setTabList(online);
-		}
-	}
-
-	public String getRRName() {
-		return settings.getConfig().getString("rr.name", "ANAMEHEREPLEASE");
-	}
-	
-	public void setRRName(String name) {
-		settings.getConfig().set("rr.name", name);
-		settings.saveConfig();
-	}
-
-	public String getHost() {
-		return settings.getConfig().getString("game.host", "None");
 	}
 
 	public boolean tier2() {
@@ -280,26 +542,6 @@ public class Game {
 	}
 
 	public boolean splash() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public int getAppleRates() {
-		// TODO Auto-generated method stub
-		return 1;
-	}
-
-	public int getPvP() {
-		// TODO Auto-generated method stub
-		return 3;
-	}
-
-	public int getMeetup() {
-		// TODO Auto-generated method stub
-		return 4;
-	}
-
-	public boolean antiStripmine() {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -315,28 +557,6 @@ public class Game {
 	}
 
 	public boolean horseArmor() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public int getMaxPlayers() {
-		return settings.getConfig().getInt("maxplayers", 150);
-	}
-
-	public String getMatchPost() {
-		return settings.getConfig().getString("matchpost", "redd.it");
-	}
-
-	public World getWorld() {
-		return Bukkit.getWorld(settings.getConfig().getString("game.world", "leon"));
-	}
-
-	public void setStats(boolean b) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public boolean stats() {
 		// TODO Auto-generated method stub
 		return false;
 	}
